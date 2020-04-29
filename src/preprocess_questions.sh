cfea07268d086de259800661f39f8e403bdbb026
AMDonati <alice.martindonati@gmail.com>
2020-04-29 20:07:07 +0200

    changing batch_size

--- src/preprocess_questions.sh
+++ src/preprocess_questions.sh
@@ -6,3 +6,12 @@ python src/preprocessing/preprocess_questions.py -data_path "data/CLEVR_v1.0/que
 
 python src/preprocessing/preprocess_questions.py -data_path "data/CLEVR_v1.0/questions/CLEVR_test_questions.json" \
 -out_vocab_path "data/vocab.json" -out_h5_path "data/test_questions.h5" -min_token_count 1
+
+python src/preprocessing/preprocess_questions.py -data_path "data/CLEVR_v1.0/temp/train_questions_50000_samples.json" \
+-out_vocab_path "data/CLEVR_v1.0/temp/vocab.json" -out_h5_path "data/CLEVR_v1.0/temp/train_questions_50000_samples.h5" -min_token_count 1
+
+python src/preprocessing/preprocess_questions.py -data_path "data/CLEVR_v1.0/temp/val_questions_20000_samples.json" \
+-out_vocab_path "data/CLEVR_v1.0/temp/vocab.json" -out_h5_path "data/CLEVR_v1.0/temp/val_questions_20000_samples.h5" -min_token_count 1
+
+python src/preprocessing/preprocess_questions.py -data_path "data/CLEVR_v1.0/temp/test_questions_20000_samples.json" \
+-out_vocab_path "data/CLEVR_v1.0/temp/vocab.json" -out_h5_path "data/CLEVR_v1.0/temp/test_questions_20000_samples.h5" -min_token_count 1
--- src/train_LM_network.sh
+++ src/train_LM_network.sh
@@ -1,3 +1,9 @@
 python src/train/train_LM_network.py -num_layers 1 -emb_size 256 \
 -hidden_size 512 -p_drop 0 -grad_clip False -data_path "data" \
 -out_path "output" -cuda True
+
+data/CLEVR_v1.0/temp/50000_20000_samples
+
+python src/train/train_LM_network.py -num_layers 1 -emb_size 64 \
+-hidden_size 128 -p_drop 0 -grad_clip False -data_path "data/CLEVR_v1.0/temp/50000_20000_samples" \
+-out_path "output" -cuda True
