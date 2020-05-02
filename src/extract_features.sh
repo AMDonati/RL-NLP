1dd0405f86069d2d9ca0dd49ff3bca7b123c60c8
AMDonati <alice.martindonati@gmail.com>
2020-05-02 09:14:22 +0200

    add arg eval in train script

--- src/extract_features.sh
+++ src/extract_features.sh
@@ -2,7 +2,7 @@ python src/preprocessing/extract_features.py \
   --input_image_dir data/CLEVR_v1.0/images/train \
   --output_h5_file data/train_features.h5 --batch_size 64
 
-python src/preprocessing/scripts/extract_features.py \
+python src/preprocessing/extract_features.py \
   --input_image_dir data/CLEVR_v1.0/images/val \
   --output_h5_file data/val_features.h5 --batch_size 64
 
--- src/train_LM_network.sh
+++ src/train_LM_network.sh
@@ -1,9 +1,8 @@
 python src/train/train_LM_network.py -num_layers 1 -emb_size 256 \
--hidden_size 512 -p_drop 0 -grad_clip False -data_path "data" \
--out_path "output" -cuda True -bs 128 -ep 10 -skip_training False
+-hidden_size 512 -p_drop 0 -grad_clip 0.5 -data_path "data" \
+-out_path "output" -cuda True -bs 512 -ep 30 -skip_training False -num_workers 5
 
-data/CLEVR_v1.0/temp/50000_20000_samples
-
+# to train on a subset of question samples.
 python src/train/train_LM_network.py -num_layers 1 -emb_size 64 \
 -hidden_size 128 -p_drop 0 -grad_clip False -data_path "data/CLEVR_v1.0/temp/50000_20000_samples" \
--out_path "output" -cuda True
+-out_path "output" -cuda True -bs 128 -ep 30 -skip_training False -num_workers 5
