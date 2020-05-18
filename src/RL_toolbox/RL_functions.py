REINFORCE: correct batch avg return formula + better monitoring + code cleaning

src/RL_toolbox/RL_functions.py
@@ -7,59 +7,35 @@ from data_provider.CLEVR_Dataset import CLEVR_Dataset
 import Levenshtein as lv
 from RL_toolbox.action_selection import select_action
 from models.Policy_network import PolicyLSTM
+from RL_toolbox.reward import Levenshtein, get_dummy_reward
 
 State = namedtuple('State', ('text', 'img'))
-Episode = namedtuple('Episode', ('img_idx', 'img_feats', 'GD_questions', 'dialog', 'rewards'))
+Episode = namedtuple('Episode', ('img_idx', 'img_feats', 'GD_questions', 'closest_question', 'dialog', 'rewards'))
 
-def get_dummy_reward(next_state_text, ep_questions, EOS_idx):
-    next_state_text = next_state_text[:, 1:]  # removing sos token.
-    if next_state_text[:, -1] == EOS_idx:  # remove <EOS> token if needed.
-        next_state_text = next_state_text[:, :-1]
-    # trunc state:
-    state_len = next_state_text.size(1)
-    if state_len == 0:
-        #print('no final reward...')
-        return 0.
-    else:
-        # max_len = ep_questions.size(1)
-        # if state_len < max_len:
-        #     next_state_text = torch.cat([next_state_text, next_state_text.new_zeros(1, max_len - state_len)], dim=1)
-        # assert max_len == next_state_text.size(1)
-        ep_questions = ep_questions[:, :state_len]
-        next_state_text = next_state_text.repeat(ep_questions.size(0), 1)
-        mask_same_tokens = next_state_text == ep_questions
-        reward = mask_same_tokens.sum(dim=-1).max().numpy()
-        reward = reward / state_len
-    return reward
 
-#TODO: batchify this function.
-def get_levenshtein_reward(next_state_text, ep_questions, dataset, EOS_idx):
-    next_state_text = next_state_text[:, 1:]  # removing sos token.
-    if next_state_text[:, -1] == EOS_idx:  # remove <EOS> token if needed.
-        next_state_text = next_state_text[:, :-1]
-    # trunc state:
-    state_len = next_state_text.size(1)
-    if state_len == 0:
-        # print('no final reward...')
-        return 0.
+def preprocess_final_state(state_text, dataset, EOS_idx):
+    state_text = state_text[:, 1:]  # removing sos token.
+    if state_text[:, -1] == EOS_idx:  # remove <EOS> token if needed.
+        state_text = state_text[:, :-1]
+    if state_text.size(1) == 0:
+        return None
     else:
-        # decode GD questions.
-        ep_questions = ep_questions.data.numpy()
-        ep_questions = [list(ep_questions[i, :10]) for i in range(ep_questions.shape[0])]
-        decoded_questions = [dataset.idx2word(question, stop_at_end=True) for question in ep_questions]
         # decode state.
-        dialog = next_state_text.view(-1).data.numpy()
+        dialog = state_text.view(-1).data.numpy()
         decoded_dialog = dataset.idx2word(list(dialog), stop_at_end=True)
-        # lv distance.
-        dist = [lv.distance(q, decoded_dialog) / max(len(q),len(decoded_dialog)) for q in decoded_questions]
-        sim = [1-d for d in dist]
+    return decoded_dialog
 
-        return max(sim)
+def preprocess_ep_questions(ep_questions, dataset, PAD_idx, max_len=None):
+    ep_questions = ep_questions.data.numpy()
+    if max_len is not None:
+        ep_questions = ep_questions[:, :max_len]
+    masks = [list(np.where(ep_questions[i,:] != PAD_idx)[0]) for i in range(ep_questions.shape[0])] # removing padding.
+    ep_questions = [list(ep_questions[i,m]) for i,m in enumerate(masks)]
+    decoded_questions = [dataset.idx2word(question, stop_at_end=True) for question in ep_questions]
+    return decoded_questions
 
 
-# function generate one episode. debugged.
-# TODO: batchify this function.
-def generate_one_episode(clevr_dataset, policy_network, special_tokens, device, max_len=None, reward_fn='lv', select='greedy', seed=None):
+def generate_one_episode(clevr_dataset, policy_network, special_tokens, device, max_len=None, select='greedy', seed=None):
     if max_len is None:
         max_len = clevr_dataset.input_questions.size(1)  # max_length set-up to max length of questions dataset (or avg len?)
     max_len = 10  # FOR DEBUGGING.
@@ -68,7 +44,7 @@ def generate_one_episode(clevr_dataset, policy_network, special_tokens, device, 
         np.random.seed(seed)
     img_idx = np.random.randint(0, len(clevr_dataset.img_idxs))
     img_idx = 0  # FOR DEBUGGING.
-    ep_GD_questions = clevr_dataset.get_questions_from_img_idx(img_idx)  # shape (10, S-1) # used to compute the final reward of the episode.
+    ep_questions = clevr_dataset.get_questions_from_img_idx(img_idx)  # shape (10, S-1) # used to compute the final reward of the episode.
     img_feats = clevr_dataset.get_feats_from_img_idx(img_idx)  # shape (1024, 14, 14)
     initial_state = State(torch.LongTensor([special_tokens.SOS_idx]).view(1, 1), img_feats.unsqueeze(0))
 
@@ -85,21 +61,18 @@ def generate_one_episode(clevr_dataset, policy_network, special_tokens, device, 
         next_state = State(torch.cat([state.text, action], dim=1), state.img)
         done = True if action.item() == special_tokens.EOS_idx or step == (max_len - 1) else False
         if done:
-            # reward = get_dummy_reward(next_state_text=next_state.text,
-            #                           ep_questions=ep_GD_questions,
-            #                           EOS_idx=special_tokens.EOS_idx)
-            reward = get_levenshtein_reward(next_state_text=next_state.text,
-                                            ep_questions=ep_GD_questions,
-                                            dataset=clevr_dataset,
-                                            EOS_idx=special_tokens.EOS_idx)
+            ep_questions_decoded = preprocess_ep_questions(ep_questions=ep_questions, dataset=clevr_dataset, PAD_idx=special_tokens.PAD_idx, max_len=max_len)
+            dialog = preprocess_final_state(state_text=next_state.text, dataset=clevr_dataset, EOS_idx=special_tokens.EOS_idx)
+            reward_fn = Levenshtein(path=None)
+            reward, closest_question = reward_fn.get(question=dialog, ep_questions_decoded=ep_questions_decoded)
         else:
-            reward = 0
+            reward = 0.
             step += 1
         rewards.append(reward)
         log_probs.append(log_prob)
         state = next_state
 
-    episode = Episode(img_idx, img_feats.data.numpy(), ep_GD_questions.data.numpy(), state.text.squeeze().data.numpy(), rewards)
+    episode = Episode(img_idx, img_feats.data.numpy(), ep_questions_decoded, closest_question, dialog, rewards)
     return_ep = sum(rewards)
     returns = [return_ep] * (step + 1)
 
@@ -108,58 +81,58 @@ def generate_one_episode(clevr_dataset, policy_network, special_tokens, device, 
 
     return log_probs, returns, episode
 
-def generate_episodes_batch(clevr_dataset, policy_network, special_tokens, device, BATCH_SIZE, max_len=None, reward_fn='lv', select='greedy', seed=None):
-    if max_len is None:
-        max_len = clevr_dataset.input_questions.size(1)  # max_length set-up to max length of questions dataset (or avg len?)
-    # sample initial state
-    if seed is not None:
-        np.random.seed(seed)
-    img_idx = list(np.random.randint(0, len(clevr_dataset), size=BATCH_SIZE))
-    ep_GD_questions = [clevr_dataset.get_questions_from_img_idx(i) for i in img_idx] #TODO: debug shape (0,45) for some idx.
-    #  shape (10, S-1) # used to compute the final reward of the episode.
-    ep_GD_questions = torch.stack(ep_GD_questions, dim=0)
-    img_feats = [clevr_dataset.get_feats_from_img_idx(i) for i in img_idx]  # shape (1024, 14, 14)
-    img_feats = torch.stack(img_feats, dim=0)
-    initial_state = State(torch.LongTensor([special_tokens.SOS_idx]).view(1, 1).repeat(BATCH_SIZE, 1), img_feats)
-
-    state = initial_state
-    done = False
-    step = 0
-    rewards, log_probs = [], []
-    while not done:
-        if select == 'sampling':
-            action, log_prob = select_action(policy_network, state, device, mode='sampling')
-        elif select == 'greedy':
-            action, log_prob = select_action(policy_network, state, device, mode='greedy') # action (bs, 1), # log_probs (bs)
-        # compute next state, done, reward from the action.
-        next_state = State(torch.cat([state.text, action], dim=1), state.img)
-        done = True if action.item() == special_tokens.EOS_idx or step == (max_len - 1) else False #TODO: adapt this to the batch case.
-        if done:
-            # reward = get_dummy_reward(next_state_text=next_state.text,
-            #                           ep_questions=ep_GD_questions,
-            #                           EOS_idx=special_tokens.EOS_idx)
-            reward_batch = []
-            for i in range(BATCH_SIZE):
-                reward = get_levenshtein_reward(next_state_text=next_state.text[i,:],
-                                            ep_questions=ep_GD_questions[i,:,:],
-                                            dataset=clevr_dataset,
-                                            EOS_idx=special_tokens.EOS_idx)
-                reward_batch.append(reward)
-        else:
-            reward_batch = [0.] * BATCH_SIZE
-            step += 1
-        rewards.append(reward_batch)
-        log_probs.append(log_prob)
-        state = next_state
-
-    episode = Episode(img_idx, img_feats.data.numpy(), ep_GD_questions.data.numpy(), state.text.data.numpy(), rewards)
-    return_ep = sum(rewards)
-    returns = [return_ep] * (step + 1)
-
-    if len(returns) < max_len:
-        assert state.text[:, -1] == special_tokens.EOS_idx
-
-    return log_probs, returns, episode
+# def generate_episodes_batch(clevr_dataset, policy_network, special_tokens, device, BATCH_SIZE, max_len=None, select='greedy', seed=None):
+#     if max_len is None:
+#         max_len = clevr_dataset.input_questions.size(1)  # max_length set-up to max length of questions dataset (or avg len?)
+#     # sample initial state
+#     if seed is not None:
+#         np.random.seed(seed)
+#     img_idx = list(np.random.randint(0, len(clevr_dataset), size=BATCH_SIZE))
+#     ep_GD_questions = [clevr_dataset.get_questions_from_img_idx(i) for i in img_idx] #TODO: debug shape (0,45) for some idx.
+#     #  shape (10, S-1) # used to compute the final reward of the episode.
+#     ep_GD_questions = torch.stack(ep_GD_questions, dim=0)
+#     img_feats = [clevr_dataset.get_feats_from_img_idx(i) for i in img_idx]  # shape (1024, 14, 14)
+#     img_feats = torch.stack(img_feats, dim=0)
+#     initial_state = State(torch.LongTensor([special_tokens.SOS_idx]).view(1, 1).repeat(BATCH_SIZE, 1), img_feats)
+#
+#     state = initial_state
+#     done = False
+#     step = 0
+#     rewards, log_probs = [], []
+#     while not done:
+#         if select == 'sampling':
+#             action, log_prob = select_action(policy_network, state, device, mode='sampling')
+#         elif select == 'greedy':
+#             action, log_prob = select_action(policy_network, state, device, mode='greedy') # action (bs, 1), # log_probs (bs)
+#         # compute next state, done, reward from the action.
+#         next_state = State(torch.cat([state.text, action], dim=1), state.img)
+#         done = True if action.item() == special_tokens.EOS_idx or step == (max_len - 1) else False #TODO: adapt this to the batch case.
+#         if done:
+#             # reward = get_dummy_reward(next_state_text=next_state.text,
+#             #                           ep_questions=ep_GD_questions,
+#             #                           EOS_idx=special_tokens.EOS_idx)
+#             reward_batch = []
+#             for i in range(BATCH_SIZE):
+#                 reward = get_levenshtein_reward(next_state_text=next_state.text[i,:],
+#                                             ep_questions=ep_GD_questions[i,:,:],
+#                                             dataset=clevr_dataset,
+#                                             EOS_idx=special_tokens.EOS_idx)
+#                 reward_batch.append(reward)
+#         else:
+#             reward_batch = [0.] * BATCH_SIZE
+#             step += 1
+#         rewards.append(reward_batch)
+#         log_probs.append(log_prob)
+#         state = next_state
+#
+#     episode = Episode(img_idx, img_feats.data.numpy(), ep_GD_questions.data.numpy(), state.text.data.numpy(), rewards)
+#     return_ep = sum(rewards)
+#     returns = [return_ep] * (step + 1)
+#
+#     if len(returns) < max_len:
+#         assert state.text[:, -1] == special_tokens.EOS_idx
+#
+#     return log_probs, returns, episode
 
 
 def padder_batch(batch):
@@ -182,26 +155,6 @@ def train_episodes_batch(log_probs_batch, returns_batch, optimizer):
     return reinforce_loss.item()
 
 if __name__ == '__main__':
-    # test of get_dummy_reward function.
-    h5_questions_path = os.path.join("../../data", 'train_questions.h5')
-    h5_feats_path = os.path.join("../../data", 'train_features.h5')
-    vocab_path = os.path.join("../../data", 'vocab.json')
-    clevr_dataset = CLEVR_Dataset(h5_questions_path=h5_questions_path,
-                                  h5_feats_path=h5_feats_path,
-                                  vocab_path=vocab_path)
-    sample_questions = clevr_dataset.get_questions_from_img_idx(0)
-    temp_state_text = torch.LongTensor([1, 7, 86, 70, 70, 21, 54, 81, 51, 84, 86, 50, 38, 17, 2]).unsqueeze(0)
-    temp_reward = get_dummy_reward(temp_state_text, sample_questions, 2)
-    print('reward', temp_reward)
-
-    # test of levenstein reward function.
-    temp_rew_lv = get_levenshtein_reward(temp_state_text, sample_questions, clevr_dataset, 2)
-    print('temp rew', temp_rew_lv)
-
-if __name__ == '__main__':
-    State = namedtuple('State', ('text', 'img'))
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    BATCH_SIZE = 8
     h5_questions_path = os.path.join("../../data", 'train_questions.h5')
     h5_feats_path = os.path.join("../../data", 'train_features.h5')
     vocab_path = os.path.join("../../data", 'vocab.json')
@@ -209,10 +162,20 @@ if __name__ == '__main__':
                                   h5_feats_path=h5_feats_path,
                                   vocab_path=vocab_path,
                                   max_samples=21)
+
+    # ---- test of get dummy reward -----------------------------------------
+    sample_questions = clevr_dataset.get_questions_from_img_idx(0)
+    temp_state_text = torch.LongTensor([1, 7, 86, 70, 70, 21, 54, 81, 51, 84, 86, 50, 38, 17, 2]).unsqueeze(0)
+    temp_reward = get_dummy_reward(temp_state_text, sample_questions, 2)
+    print('reward', temp_reward)
+
+    State = namedtuple('State', ('text', 'img'))
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     SOS_idx = clevr_dataset.vocab_questions["<SOS>"]
     EOS_idx = clevr_dataset.vocab_questions["<EOS>"]
-    Special_Tokens = namedtuple('Special_Tokens', ('SOS_idx', 'EOS_idx'))
-    special_tokens = Special_Tokens(SOS_idx, EOS_idx)
+    PAD_idx = clevr_dataset.vocab_questions["<PAD>"]
+    Special_Tokens = namedtuple('Special_Tokens', ('SOS_idx', 'EOS_idx', 'PAD_idx'))
+    special_tokens = Special_Tokens(SOS_idx, EOS_idx, PAD_idx)
 
     policy_network = PolicyLSTM(num_tokens=87,
                                 word_emb_size=16,
@@ -221,9 +184,22 @@ if __name__ == '__main__':
                                 num_layers=1,
                                 p_drop=0)
 
-    # -------- test of generate_episode_batch ----------------------------------------------------------------------------------------
-    log_probs, action, episode = generate_episodes_batch(clevr_dataset=clevr_dataset,
+    # -------- test of generate_episode  ----------------------------------------------------------------------------------------
+    log_probs, returns, episode = generate_one_episode(clevr_dataset=clevr_dataset,
                                                          policy_network=policy_network,
                                                          special_tokens=special_tokens,
-                                                         device=device,
-                                                         BATCH_SIZE=BATCH_SIZE)
+                                                         device=device)
+    print('log_probs', len(log_probs))
+    print('return', returns[-1])
+    print('dialog', episode.dialog)
+    print('closest question', episode.closest_question)
+
+    # ------- test of preprocess functions -------------------------------------------------------------------------------------------
+    #temp_state = torch.tensor([[1,2]])
+    temp_state = torch.tensor([[1,10, 20, 62,2]])
+    temp_q = torch.tensor([[13,22,34,10,7,0,0,0,0,0],[67, 45, 63, 0, 0, 0, 0, 0, 0, 0]])
+    temp_dialog = preprocess_final_state(state_text=temp_state, dataset=clevr_dataset, EOS_idx=special_tokens.EOS_idx)
+    temp_questions = preprocess_ep_questions(ep_questions=temp_q, dataset=clevr_dataset, PAD_idx=PAD_idx, max_len=10)
+    print('temp question', temp_questions[0])
+    print('temp question', temp_questions[1])
+    print('temp dialog', temp_dialog)

src/RL_toolbox/action_selection.py
@@ -46,9 +46,8 @@ def select_action_batch(policy_network, state, device, mode='greedy'):
     return action.view(bs, 1), log_prob
 
 
-
-
 if __name__ == '__main__':
+
     State = namedtuple('State', ('text', 'img'))
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     BATCH_SIZE = 1

src/RL_toolbox/reward.py
@@ -41,12 +41,39 @@ class Levenshtein(Reward):
         return -min(distances)
 
     def get(self, question, ep_questions_decoded):
-        distances = [nltk.edit_distance(question.split(), true_question.split()) / (max(len(question.split()), len(true_question.split()))) for true_question in
-                     ep_questions_decoded]
-        similarities = [1-dist for dist in distances]
-        return max(similarities)
+        if question is None:
+            return 0., None
+        else:
+            distances = [nltk.edit_distance(question.split(), true_question.split()) / (max(len(question.split()), len(true_question.split()))) for true_question in
+                         ep_questions_decoded]
+            similarities = [1-dist for dist in distances]
+            sim_question_idx = np.asarray(similarities).argmax()
+            closest_question = ep_questions_decoded[sim_question_idx]
+
+            return max(similarities), closest_question
 
 rewards = {"cosine": Cosine, "levenshtein": Levenshtein}
+
+def get_dummy_reward(next_state_text, ep_questions, EOS_idx):
+    next_state_text = next_state_text[:, 1:]  # removing sos token.
+    if next_state_text[:, -1] == EOS_idx:  # remove <EOS> token if needed.
+        next_state_text = next_state_text[:, :-1]
+    # trunc state:
+    state_len = next_state_text.size(1)
+    if state_len == 0:
+        #print('no final reward...')
+        return 0.
+    else:
+        # max_len = ep_questions.size(1)
+        # if state_len < max_len:
+        #     next_state_text = torch.cat([next_state_text, next_state_text.new_zeros(1, max_len - state_len)], dim=1)
+        # assert max_len == next_state_text.size(1)
+        ep_questions = ep_questions[:, :state_len]
+        next_state_text = next_state_text.repeat(ep_questions.size(0), 1)
+        mask_same_tokens = next_state_text == ep_questions
+        reward = mask_same_tokens.sum(dim=-1).max().numpy()
+        reward = reward / state_len
+    return reward
 
 if __name__ == '__main__':
     reward_func = rewards["cosine"](path="../../data/CLEVR_v1.0/temp/50000_20000_samples_old/train_questions.json")
@@ -64,7 +91,7 @@ if __name__ == '__main__':
     rew = reward_func.get("is it blue ?", ["is it blue ?", "where is it ?"])
     print("reward {} levenshtein".format(rew))
 
-    str_1 = "is it blue and tiny?"
+    str_1 = "is it blue and tiny ?"
     str_2 = "is it blue ?"
 
     rew = reward_func.get_old(str_1, [str_2])
@@ -72,10 +99,11 @@ if __name__ == '__main__':
     print('rew negative ', rew)
     print('rew norm negative', rew_norm)
 
-    rew_norm_pos = reward_func.get(str_1, [str_2])
+    rew_norm_pos, sim_q = reward_func.get(str_1, [str_2])
     print('rew norm positive', rew_norm_pos)
+    print('similar question', sim_q)
 
-    str_1 = "is it red?"
+    str_1 = "is it red ?"
     str_2 = "is it blue ?"
 
 
@@ -84,5 +112,6 @@ if __name__ == '__main__':
     print('rew', rew)
     print('rew norm', rew_norm)
 
-    rew_norm_pos = reward_func.get(str_1, [str_2])
+    rew_norm_pos, sim_q = reward_func.get(str_1, [str_2])
     print('rew norm positive', rew_norm_pos)
+    print('similar question', sim_q)

src/models/Policy_network.py
@@ -11,7 +11,7 @@ import numpy as np
 
 class PolicyLSTM(nn.Module):
     def __init__(self, num_tokens, word_emb_size, emb_size, hidden_size, num_filters=None, num_layers=1, p_drop=0,
-                 pooling=True, project=False):
+                 pooling=True):
         super(PolicyLSTM, self).__init__()
         self.num_tokens = num_tokens
         self.emb_size = emb_size
@@ -21,23 +21,14 @@ class PolicyLSTM(nn.Module):
         if num_filters is None:
             self.num_filters = word_emb_size
         else:
-            self.num_filters = num_filters
+            self.num_filters = num_filters #TODO add a min between 64 and word_emb_size.
         self.pooling = pooling
-        self.project = project
         self.dropout = nn.Dropout(p_drop)
         self.embedding = nn.Embedding(num_tokens, word_emb_size)
         self.conv = nn.Conv2d(in_channels=1024, out_channels=self.num_filters, kernel_size=1)
         if pooling:
             self.max_pool = nn.MaxPool2d(kernel_size=2)
-        if project:
-            self.fc_emb = nn.Linear(emb_size, word_emb_size)
-            self.lstm = nn.LSTM(input_size=word_emb_size,
-                            hidden_size=hidden_size,
-                            num_layers=num_layers,
-                            dropout=p_drop,
-                            batch_first=True)
-        else:
-            self.lstm = nn.LSTM(input_size=emb_size,
+        self.lstm = nn.LSTM(input_size=emb_size,
                                 hidden_size=hidden_size,
                                 num_layers=num_layers,
                                 dropout=p_drop,
@@ -60,11 +51,8 @@ class PolicyLSTM(nn.Module):
         img_feat = F.relu(self.conv(img_feat))
         if self.pooling:
             img_feat = self.max_pool(img_feat)
-
         img_feat = img_feat.view(img_feat.size(0), -1).unsqueeze(1).repeat(1, seq_len, 1)  # shape (B, S, C*H*W)
         vis_text_emb = torch.cat([words_emb, img_feat], axis=-1)
-        if self.project:
-            vis_text_emb = F.relu(self.fc_emb(vis_text_emb))
         output, hidden = self.lstm(vis_text_emb)
         output = self.dropout(output)
         logits = self.fc(output)  # (S,B,num_tokens)

src/train/train_Policy_RL.py
@@ -1,9 +1,14 @@
 # https://pytorch.org/tutorials/intermediate/tensorboard_tutorial.html?highlight=tensorboard
 # example of ROUGE computation: https://github.com/atulkum/pointer_summarizer/blob/master/data_util/utils.py
-from models.Policy_network import PolicyLSTM, PolicyMLP
+import os
+import torch
+import numpy as np
 import argparse
+from collections import namedtuple
+from models.Policy_network import PolicyLSTM, PolicyMLP
+from data_provider.CLEVR_Dataset import CLEVR_Dataset
 from utils.utils_train import create_logger, write_to_csv
-from RL_toolbox.RL_functions import *
+from RL_toolbox.RL_functions import generate_one_episode, preprocess_ep_questions, padder_batch, train_episodes_batch
 from torch.utils.tensorboard import SummaryWriter
 
 
@@ -28,7 +33,7 @@ if __name__ == '__main__':
     parser.add_argument("-p_drop", type=float, default=0, help="dropout rate")
     parser.add_argument("-grad_clip", type=float)
     parser.add_argument("-lr", type=float, default=0.001)
-    parser.add_argument("-bs", type=int, default=16, help="batch size")
+    parser.add_argument("-bs", type=int, default=1, help="batch size")
     parser.add_argument("-max_len", type=int, default=10, help="max episode length")
     parser.add_argument("-num_training_steps", type=int, default=100000, help="number of training_steps")
     parser.add_argument("-data_path", type=str, required=True,
@@ -36,7 +41,7 @@ if __name__ == '__main__':
     parser.add_argument("-out_path", type=str, required=True, help="out folder")
     parser.add_argument('-num_workers', type=int, default=0, help="num workers for DataLoader")
     parser.add_argument('-pre_train', type=str2bool, default=False, help="pre-train the policy network with SL.")
-    parser.add_argument('-model_path', type=str, default='../../output/SL_1/model.pt', help="path for the pre-trained model with SL")
+    parser.add_argument('-model_path', type=str, default='../../output/SL_32_64/model.pt', help="path for the pre-trained model with SL")
     args = parser.parse_args()
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
@@ -47,19 +52,21 @@ if __name__ == '__main__':
     h5_questions_path = os.path.join(args.data_path, 'train_questions.h5')
     h5_feats_path = os.path.join(args.data_path, 'train_features.h5')
     vocab_path = os.path.join(args.data_path, 'vocab.json')
-    clevr_dataset = CLEVR_Dataset(h5_questions_path=h5_questions_path,
+    train_dataset = CLEVR_Dataset(h5_questions_path=h5_questions_path,
                                   h5_feats_path=h5_feats_path,
-                                  vocab_path=vocab_path)
+                                  vocab_path=vocab_path,
+                                  max_samples=21)
 
-    num_tokens = clevr_dataset.len_vocab
-    feats_shape = clevr_dataset.feats_shape
-    SOS_idx = clevr_dataset.vocab_questions["<SOS>"]
-    EOS_idx = clevr_dataset.vocab_questions["<EOS>"]
+    num_tokens = train_dataset.len_vocab
+    feats_shape = train_dataset.feats_shape
+    SOS_idx = train_dataset.vocab_questions["<SOS>"]
+    EOS_idx = train_dataset.vocab_questions["<EOS>"]
+    PAD_idx = train_dataset.vocab_questions["<PAD>"]
 
-    Special_Tokens = namedtuple('Special_Tokens', ('SOS_idx', 'EOS_idx'))
-    special_tokens = Special_Tokens(SOS_idx, EOS_idx)
+    Special_Tokens = namedtuple('Special_Tokens', ('SOS_idx', 'EOS_idx', 'PAD_idx'))
+    special_tokens = Special_Tokens(SOS_idx, EOS_idx, PAD_idx)
     State = namedtuple('State', ('text', 'img'))
-    Episode = namedtuple('Episode', ('img_idx', 'img_feats', 'GD_questions', 'dialog', 'rewards'))
+    Episode = namedtuple('Episode', ('img_idx', 'img_feats', 'GD_questions', 'closest_question', 'dialog', 'rewards'))
 
     ##################################################################################################################
     # Build the Policy Network and define hparams
@@ -96,9 +103,6 @@ if __name__ == '__main__':
     csv_out_file = os.path.join(output_path, 'train_history.csv')
     model_path = os.path.join(output_path, 'model.pt')
 
-
-    train_dataset = clevr_dataset
-    store_episodes = True
     log_interval = 10
 
     #####################################################################################################################
@@ -111,16 +115,15 @@ if __name__ == '__main__':
     writer = SummaryWriter(log_dir=os.path.join(output_path, 'runs'))
 
     # Get and print set of questions for the fixed img.
-    ep_questions = train_dataset.get_questions_from_img_idx(0).data.numpy()
-    ep_questions = [list(ep_questions[i, :10]) for i in range(ep_questions.shape[0])]
-    decoded_questions = [train_dataset.idx2word(question, stop_at_end=True) for question in ep_questions]
-    logger.info('RL from scratch with levenshtein reward on Image #0, with episode max length = 10')
-    logger.info("episode questions (10tokens):")
-    logger.info('{}'.format('\n').join(decoded_questions))
+    ep_questions = train_dataset.get_questions_from_img_idx(0)
+    ep_questions_decoded = preprocess_ep_questions(ep_questions=ep_questions, dataset=train_dataset, PAD_idx=special_tokens.PAD_idx)
+    logger.info('RL from scratch with word-level levenshtein reward on Image #0, with episode max length = 10')
+    logger.info("episode questions (full length):")
+    logger.info('{}'.format('\n').join(ep_questions_decoded))
 
     for i in range(args.num_training_steps):
         log_probs_batch, returns_batch, episodes_batch = [], [], []
-        for _ in range(args.bs):
+        for batch in range(args.bs):
             log_probs, returns, episode = generate_one_episode(clevr_dataset=train_dataset,
                                                                policy_network=policy_network,
                                                                special_tokens=special_tokens,
@@ -128,31 +131,35 @@ if __name__ == '__main__':
                                                                device=device)
             log_probs_batch.append(log_probs)
             returns_batch.append(returns)
-            if store_episodes:
-                episodes_batch.append(episode)
+            episodes_batch.append(episode)
 
-        log_probs_batch = padder_batch(log_probs_batch)
-        returns_batch = padder_batch(returns_batch)
-        batch_avg_return = returns_batch[:, -1, :].mean(0).squeeze().data.numpy()
+        # getting return statistics before padding.
+        return_batch = [r[-1] for r in returns_batch]
+        batch_avg_return = sum(return_batch) / len(return_batch)
+        batch_max_return, max_id = max(return_batch), np.asarray(return_batch).argmax()
+        max_dialog, closest_question = episodes_batch[max_id].dialog, episodes_batch[max_id].closest_question
+
+        log_probs_batch = padder_batch(log_probs_batch) # shape (bs, max_len, 1)
+        returns_batch = padder_batch(returns_batch) # shape (bs, max_len, 1)
         loss = train_episodes_batch(log_probs_batch=log_probs_batch, returns_batch=returns_batch, optimizer=optimizer)
         sum_loss += loss
         running_return = 0.1 * batch_avg_return + (1 - 0.1) * running_return
 
         if i % log_interval == log_interval - 1:
             # writing metrics to tensorboard.
-            writer.add_scalar('batch return',batch_avg_return,i)
-            writer.add_scalar('running return', running_return,i)
-            if store_episodes and i == log_interval - 1:
-                writer.add_text('episode_questions(10tokens)', ('...').join(decoded_questions))
+            writer.add_scalar('batch return',batch_avg_return,i+1)
+            writer.add_scalar('running return', running_return,i+1)
+            if i == log_interval - 1:
+                writer.add_text('episode_questions', ('...').join(ep_questions_decoded))
 
         if i % (10*log_interval) == (10*log_interval - 1):
             logger.info('train loss for training step {}: {:5.3f}'.format(i, loss))
             logger.info('average batch return for training step {}: {:5.3f}'.format(i, batch_avg_return))
             logger.info('running return for training step {}: {:8.3f}'.format(i, loss / (i + 1)))
+
             writer.add_scalar('training loss', sum_loss / (i + 1), i)
-            dialog_batch = [list(ep.dialog) for ep in episodes_batch]
-            decoded_dialog_batch = [train_dataset.idx2word(dialog, stop_at_end=True) for dialog in dialog_batch]
-            writer.add_text('dialog_batch', ('...\t').join(decoded_dialog_batch), global_step=i)
+            writer.add_text('best current dialog for return: {}'.format(batch_max_return), max_dialog, global_step=i+1)
+            writer.add_text('closest question', closest_question, global_step=i+1)
 
             sum_loss = 0. #resetting loss.
             with open(model_path, 'wb') as f:
@@ -161,8 +168,6 @@ if __name__ == '__main__':
             loss_hist.append(loss / (i + 1))
             batch_return_hist.append(batch_avg_return)
             running_return_hist.append(running_return)
-
-        if store_episodes:
             all_episodes.append(episodes_batch)
 
     hist_keys = ['loss', 'return_batch', 'running_return']