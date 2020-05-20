4dca4fc58e31ecc53400306559f5dc7b151f79e5
Guillaume Quispe <guillaume.quispe@telecom-paristech.fr>
2020-05-20 13:39:30 +0200

    comment preprocess, code in idx2word?

--- src/envs/clevr_env.py
+++ src/envs/clevr_env.py
@@ -5,7 +5,6 @@ import gym
 import numpy as np
 import torch
 
-from RL_toolbox.RL_functions import preprocess_final_state
 from RL_toolbox.reward import rewards
 from data_provider.CLEVR_Dataset import CLEVR_Dataset
 
@@ -56,8 +55,8 @@ class ClevrEnv(gym.Env):
         self.state = self.State(torch.cat([self.state.text, action], dim=1), self.state.img)
         question = self.clevr_dataset.idx2word(self.state.text.numpy()[0])
         done = True if action.item() == self.special_tokens.EOS_idx or self.step_idx == (self.max_len - 1) else False
-        question = preprocess_final_state(state_text=self.state.text, dataset=self.clevr_dataset,
-                                          EOS_idx=self.special_tokens.EOS_idx)
+        # question = preprocess_final_state(state_text=self.state.text, dataset=self.clevr_dataset,
+        #                                  EOS_idx=self.special_tokens.EOS_idx)
         reward, closest_question = self.reward_func.get(question=question,
                                                         ep_questions_decoded=self.ref_questions_decoded) if done else (
             0, None)
