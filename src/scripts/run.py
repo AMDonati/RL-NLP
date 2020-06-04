9775fdd045dcc86d3a847db99f248b14a6798068
Guillaume Quispe <guillaume.quispe@telecom-paristech.fr>
2020-06-04 18:18:53 +0200

    refactor

--- src/scripts/run.py
+++ src/scripts/run.py
@@ -11,6 +11,55 @@ from envs.clevr_env import ClevrEnv
 from models.rl_basic import PolicyLSTMWordBatch, PolicyLSTMBatch
 from utils.utils_train import create_logger
 
+
+def main(args):
+    output_path = os.path.join(args.out_path, "experiments", "train",
+                               "{}".format(datetime.datetime.now().strftime("%Y%m%d-%H%M%S")))
+    if not os.path.isdir(output_path):
+        os.makedirs(output_path)
+    out_file_log = os.path.join(output_path, 'RL_training_log.log')
+    out_policy_file = os.path.join(output_path, 'model.pth')
+
+    logger = create_logger(out_file_log, level=args.logger_level)
+
+    writer = SummaryWriter(log_dir=os.path.join(output_path, "runs_{}_{}_{}_{}_{}".format(args.model, args.eps_clip,
+                                                                                          args.num_filters, args.stride,
+                                                                                          args.conv_kernel)))
+
+    env = ClevrEnv(args.data_path, args.max_len, reward_type=args.reward, mode="train", debug=args.debug)
+
+    pretrained_lm = None
+    if args.pretrained_path is not None:
+        pretrained_lm = torch.load(args.pretrained_path)
+        pretrained_lm.eval()
+
+    models = {
+        "lstm": PolicyLSTMBatch,
+        "lstm_word": PolicyLSTMWordBatch}
+
+    generic_kwargs = {"pretrained_lm": pretrained_lm, "pretrain": args.pretrain, "word_emb_size": args.word_emb_size,
+                      "hidden_size": args.hidden_size, "kernel_size": args.conv_kernel, "stride": args.stride,
+                      "num_filters": args.num_filters, "num_truncated": args.num_truncated}
+
+    ppo_kwargs = {"policy": models[args.model], "env": env, "gamma": args.gamma,
+                  "K_epochs": args.K_epochs,
+                  "update_timestep": args.update_timestep, "entropy_coeff": args.entropy_coeff,
+                  "eps_clip": args.eps_clip}
+    reinforce_kwargs = {"env": env, "policy": models[args.model], "gamma": args.gamma, "lr": args.lr,
+                        "word_emb_size": args.word_emb_size, "hidden_size": args.hidden_size}
+    algo_kwargs = {"PPO": ppo_kwargs, "REINFORCE": reinforce_kwargs}
+    kwargs = {**algo_kwargs[args.agent], **generic_kwargs}
+
+    agents = {"PPO": PPO, "REINFORCE": REINFORCE}
+
+    agent = agents[args.agent](**kwargs)
+
+    agent.learn(log_interval=args.log_interval, num_episodes=args.num_episodes_train,
+                writer=writer, output_path=output_path)
+    agent.save(out_policy_file)
+    agent.test(log_interval=args.log_interval, num_episodes=args.num_episodes_test, writer=writer)
+
+
 if __name__ == '__main__':
     # -data_path /Users/guillaumequispe/PycharmProjects/RL-NLP/data -out_path /Users/guillaumequispe/PycharmProjects/RL-NLP/output
     # -max_len 7 -logger_level DEBUG -num_episodes_train 4000 -log_interval 1 -reward "levenshtein_"
@@ -49,53 +98,4 @@ if __name__ == '__main__':
     parser.add_argument('-num_truncated', type=int, default=10, help="number of words from lm")
 
     args = parser.parse_args()
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
-    output_path = os.path.join(args.out_path, "experiments", "train",
-                               "{}".format(datetime.datetime.now().strftime("%Y%m%d-%H%M%S")))
-    if not os.path.isdir(output_path):
-        os.makedirs(output_path)
-    out_file_log = os.path.join(output_path, 'RL_training_log.log')
-    out_policy_file = os.path.join(output_path, 'model.pth')
-
-    logger = create_logger(out_file_log, level=args.logger_level)
-
-    writer = SummaryWriter(log_dir=os.path.join(output_path, "runs_{}_{}_{}_{}_{}".format(args.model, args.eps_clip,
-                                                                                          args.num_filters, args.stride,
-                                                                                          args.conv_kernel)))
-
-    env = ClevrEnv(args.data_path, args.max_len, reward_type=args.reward, mode="train", debug=args.debug)
-
-    # make_env_fn = lambda: ClevrEnv(args.data_path, args.max_len, reward_type=args.reward, mode="train", debug=True)
-    # envs = VectorEnv(make_env_fn, n=2)
-
-    pretrained_lm = None
-    if args.pretrained_path is not None:
-        pretrained_lm = torch.load(args.pretrained_path)
-        pretrained_lm.eval()
-
-    models = {
-        "lstm": PolicyLSTMBatch,
-        "lstm_word": PolicyLSTMWordBatch}
-
-    generic_kwargs = {"pretrained_lm": pretrained_lm, "pretrain": args.pretrain, "word_emb_size": args.word_emb_size,
-                      "hidden_size": args.hidden_size, "kernel_size": args.conv_kernel, "stride": args.stride,
-                      "num_filters": args.num_filters, "num_truncated": args.num_truncated}
-
-    ppo_kwargs = {"policy": models[args.model], "env": env, "gamma": args.gamma,
-                  "K_epochs": args.K_epochs,
-                  "update_timestep": args.update_timestep, "entropy_coeff": args.entropy_coeff,
-                  "eps_clip": args.eps_clip}
-    reinforce_kwargs = {"env": env, "policy": models[args.model], "gamma": args.gamma, "lr": args.lr,
-                        "word_emb_size": args.word_emb_size, "hidden_size": args.hidden_size}
-    algo_kwargs = {"PPO": ppo_kwargs, "REINFORCE": reinforce_kwargs}
-    kwargs = {**algo_kwargs[args.agent], **generic_kwargs}
-
-    agents = {"PPO": PPO, "REINFORCE": REINFORCE}
-
-    agent = agents[args.agent](**kwargs)
-
-    agent.learn(log_interval=args.log_interval, num_episodes=args.num_episodes_train,
-                writer=writer, output_path=output_path)
-    agent.save(out_policy_file)
-    agent.test(log_interval=args.log_interval, num_episodes=args.num_episodes_test, writer=writer)
+    main(args)
