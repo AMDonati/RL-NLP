e8ec4476549b3abbe30cab4f971afd68b64d2e8d
Guillaume Quispe <guillaume.quispe@telecom-paristech.fr>
2020-07-02 01:15:15 +0200

    add metric

--- src/agent/agent.py
+++ src/agent/agent.py
@@ -5,6 +5,8 @@ import torch
 import torch.optim as optim
 from nltk.translate.bleu_score import sentence_bleu
 
+from eval.metric import PPLMetric, RewardMetric, LMMetric
+
 
 class Memory:
     def __init__(self):
@@ -29,7 +31,8 @@ class Memory:
 
 
 class Agent:
-    def __init__(self, policy, env, writer, gamma=1., lr=1e-2, pretrained_lm=None, pretrained_policy=None, pretrain=False,
+    def __init__(self, policy, env, writer, gamma=1., lr=1e-2, pretrained_lm=None, pretrained_policy=None,
+                 pretrain=False,
                  update_every=50, word_emb_size=8, hidden_size=24, kernel_size=1, stride=2, num_filters=3,
                  num_truncated=10):
         self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -49,6 +52,7 @@ class Agent:
         self.num_truncated = num_truncated
         self.writer = writer
         self.generated_text = []
+        self.metrics = [PPLMetric(self), RewardMetric(self), LMMetric(self)]
 
     def get_top_k_words(self, state_text, top_k=10):
         """
@@ -87,7 +91,8 @@ class Agent:
         return diversity_metric
 
     def get_bleu_score(self, question):
-        question_decoded = self.env.clevr_dataset.idx2word(question.squeeze().cpu().numpy().tolist(), ignored=["<SOS>"], stop_at_end=True)
+        question_decoded = self.env.clevr_dataset.idx2word(question.squeeze().cpu().numpy().tolist(), ignored=["<SOS>"],
+                                                           stop_at_end=True)
         ref_questions = [q.split() for q in self.env.ref_questions_decoded]
         question_tokens = question_decoded.split()
         score = sentence_bleu(ref_questions, question_tokens)
@@ -95,54 +100,29 @@ class Agent:
 
     def test(self, log_interval=1, num_episodes=10):
         self.generated_text = []
-        log_probs_ppl = []
         self.policy.eval()
         running_reward, idx_step = 0, 0
         for i_episode in range(num_episodes):
-            top_words = []
             for ref_question in self.env.ref_questions:
                 state, ep_reward = self.env.reset(), 0
                 for t in range(0, self.env.max_len):
                     action, log_probs, value, valid_actions, dist = self.select_action(state=state,
                                                                                        num_truncated=self.num_truncated)
-                    state_decoded = self.env.clevr_dataset.idx2word(state.text.numpy()[0])
-                    top_k_weights, top_k_indices = torch.topk(dist.probs, self.num_truncated, sorted=True)
-                    if self.pretrained_lm != None:
-                        top_k_indices = torch.gather(valid_actions, 1, top_k_indices)
-                    top_words_decoded = self.env.clevr_dataset.idx2word(top_k_indices.cpu().numpy()[0])
-                    weights_words = ["{}/{:.3f}".format(word, weight, number=3) for word, weight in
-                                     zip(top_words_decoded.split(), top_k_weights[0].cpu().detach().numpy())]
-                    top_words.append("next possible words for {} : {}".format(state_decoded, ", ".join(weights_words)))
-                    if self.pretrained_lm is None:
-                        target_word_log_prob = dist.log_prob(ref_question[t].to(self.device))
-                    else:
-                        if ref_question[t].to(self.device) in valid_actions:
-                            target_word = list(valid_actions.view(-1).cpu().numpy()).index(ref_question[t])
-                            target_word_log_prob = dist.log_prob(torch.tensor([target_word]).float().to(self.device))
-                        else:
-                            # case where the target word is not in the top words of the language model
-                            target_word_log_prob = torch.tensor([-10]).float().to(self.device) #TODO: remove the else.
-                    log_probs_ppl.append(target_word_log_prob)
                     idx_step += 1
                     state, (reward, _), done, _ = self.env.step(action)
-                    ep_reward += reward
+                    for metric in self.metrics:
+                        metric.fill(state=state, done=done, dist=dist, valid_actions=valid_actions,
+                                    ref_question=ref_question, reward=reward)
                     if done:
-                        self.writer.add_scalar('test_TTR', self.get_metrics(state.text), i_episode + 1)
-                        self.writer.add_scalar('test_BLEU', self.get_bleu_score(state.text), i_episode + 1)
                         break
-            ppl = torch.exp(-torch.stack(log_probs_ppl).sum() / idx_step)
-            running_reward = 0.05 * ep_reward + (1 - 0.05) * running_reward
+            for metric in self.metrics:
+                metric.compute()
             if i_episode % log_interval == 0:
-                logging.info('Episode {}\tLast reward: {:.2f}\tAverage reward: {:.2f}'.format(
-                    i_episode, ep_reward, running_reward))
-                self.writer.add_text('episode_questions', '  \n'.join(self.env.ref_questions_decoded))
-                #TODO: add generated dialog.
-                #TODO: add ratio of unique closest question
-                #TODO: add %age of unvalid actions per episode. (counter.)
-                self.writer.add_scalar('test_running_return', running_reward, i_episode + 1)
-                self.writer.add_scalar('ppl', ppl, i_episode + 1)
-                self.writer.add_text('language_model', '  \n'.join(top_words)) #TODO: add it only with the language model.
-
+                for metric in self.metrics:
+                    metric.write()
+                # TODO: add generated dialog.
+                # TODO: add ratio of unique closest question
+                # TODO: add %age of unvalid actions per episode. (counter.)
 
     def learn(self, log_interval=10, num_episodes=100):
 
@@ -182,5 +162,3 @@ class Agent:
                     i_episode, ep_reward, running_reward))
                 self.writer.add_text('episode_questions', '  \n'.join(self.env.ref_questions_decoded))
                 self.writer.add_scalar('train_running_return', running_reward, i_episode + 1)
-
-
---
+++ src/eval/metric.py
@@ -0,0 +1,97 @@
+import numpy as np
+import torch
+
+
+class Metric:
+    def __init__(self, agent):
+        self.measure = []
+        self.metric = []
+        self.idx_step = 0
+        self.idx_word = 0
+        self.idx_write = 1
+        self.agent = agent
+
+    def fill(self, **kwargs):
+        raise NotImplementedError
+
+    def compute(self, **kwargs):
+        raise NotImplementedError
+
+    def reset(self):
+        self.idx_word = 0
+
+    def write(self, **kwargs):
+        if self.type == "scalar":
+            self.agent.writer.add_scalar(self.key, np.mean(self.metric), self.idx_write)
+        else:
+            self.agent.writer.add_text(self.key, '  \n'.join(self.metric), self.idx_write)
+        self.idx_write += 1
+
+class LMMetric(Metric):
+    def __init__(self, agent):
+        Metric.__init__(self, agent)
+        self.type = "text"
+        self.key = "lm"
+
+    def fill(self, **kwargs):
+        state_decoded = self.agent.env.clevr_dataset.idx2word(kwargs["state"].text.numpy()[0])
+        top_k_weights, top_k_indices = torch.topk(kwargs["dist"].probs, self.agent.num_truncated, sorted=True)
+        if self.agent.pretrained_lm != None:
+            top_k_indices = torch.gather(kwargs["valid_actions"], 1, top_k_indices)
+        top_words_decoded = self.agent.env.clevr_dataset.idx2word(top_k_indices.cpu().numpy()[0])
+        weights_words = ["{}/{:.3f}".format(word, weight, number=3) for word, weight in
+                         zip(top_words_decoded.split(), top_k_weights[0].cpu().detach().numpy())]
+        self.measure.append("next possible words for {} : {}".format(state_decoded, ", ".join(weights_words)))
+
+    def compute(self, **kwargs):
+        pass
+
+
+class PPLMetric(Metric):
+    def __init__(self, agent):
+        Metric.__init__(self, agent)
+        self.type = "scalar"
+        self.key = "ppl"
+
+    def fill(self, **kwargs):
+        if kwargs["done"]:
+            self.reset()
+            pass
+        if self.agent.pretrained_lm is None:
+            target_word_log_prob = kwargs["dist"].log_prob(kwargs["ref_question"][self.idx_word].to(self.agent.device))
+            self.measure.append(target_word_log_prob)
+        else:
+            if kwargs["ref_question"].to(self.agent.device) in kwargs["valid_actions"]:
+                target_word = list(kwargs["valid_actions"].view(-1).cpu().numpy()).index(
+                    kwargs["ref_question"][self.idx_word])
+                target_word_log_prob = kwargs["dist"].log_prob(
+                    torch.tensor([target_word]).float().to(self.agent.device))
+                self.measure.append(target_word_log_prob)
+            # else:
+            # case where the target word is not in the top words of the language model
+            # target_word_log_prob = torch.tensor([-10]).float().to(self.device) #TODO: remove the else.
+        self.idx_word += 1
+        self.idx_step += 1
+
+    def compute(self, **kwargs):
+        ppl = torch.exp(-torch.stack(self.measure).sum() / self.idx_step)
+        self.metric.append(ppl)
+
+    def write(self, **kwargs):
+        metric = torch.mean(torch.stack(self.metric))
+        self.agent.writer.add_scalar(self.key, metric, self.idx_write)
+        self.idx_write += 1
+
+
+class RewardMetric(Metric):
+    def __init__(self, agent):
+        Metric.__init__(self, agent)
+        self.type = "scalar"
+        self.key = "test_reward"
+
+    def fill(self, **kwargs):
+        self.idx_word += 1
+        self.measure.append(kwargs["reward"])
+
+    def compute(self, **kwargs):
+        self.metric.append(np.mean(self.measure))
