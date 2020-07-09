18b7c536cfd4a4dd2a7980c7ecefffe3ded1a021
Guillaume Quispe <guillaume.quispe@telecom-paristech.fr>
2020-07-09 03:56:30 +0200

    first version reinforce

--- src/agent/reinforce.py
+++ src/agent/reinforce.py
@@ -1,7 +1,6 @@
 import numpy as np
 import torch
 import torch.nn as nn
-from torch.nn.utils.rnn import pad_sequence
 
 from agent.agent import Agent
 
@@ -29,47 +28,33 @@ class REINFORCE(Agent):
         return action.cpu().numpy(), log_prob, value, (valid_actions, actions_probs), m
 
     def update(self):
+        # getting the lengths for the loss split
         is_terminals = np.array(self.memory.is_terminals)
-        lengths_idx = np.argwhere(is_terminals == True) + 1
-        sub_lengths_idx = np.roll(lengths_idx, shift=1)
-        sub_lengths_idx[0] = 0
-        lengths=lengths_idx-sub_lengths_idx
-
-        logprobs = torch.stack(self.memory.logprobs).to(self.device)
-
-        values = torch.stack(self.memory.values)
-        states_text = pad_sequence(self.memory.states_text, batch_first=True)
-        states_img = torch.stack(self.memory.states_img)
-        actions = torch.LongTensor(self.memory.actions)
-
-        #logprobs_split=torch.split(logprobs, tuple(lengths.flatten()))
-
-
-        # dist, _, values=self.policy(states_text, states_img)
-        # log_probs_=torch.log(dist.probs)
-        # log_probs=log_probs_[np.arange(len(actions)), actions].unsqueeze(dim=0)
+        lengths = np.argwhere(is_terminals == True) + 1
+        lengths[1:] = lengths[1:] - lengths[:-1]
 
         rewards = []
         discounted_reward = 0
-        log_probs = [[]]
-        for reward, is_terminal, log_prob in zip(reversed(self.memory.rewards), reversed(self.memory.is_terminals),
-                                                 reversed(self.memory.logprobs)):
+        for reward, is_terminal in zip(reversed(self.memory.rewards), reversed(self.memory.is_terminals)):
             discounted_reward = reward + (self.gamma * discounted_reward * (1 - is_terminal))
             rewards.insert(0, discounted_reward)
 
         rewards = torch.tensor(rewards).to(self.device).float().view(-1, 1)
+        logprobs = torch.stack(self.memory.logprobs).to(self.device)
+        values = torch.stack(self.memory.values)
         advantages = rewards.squeeze() - values.squeeze()
-        #advantages_split=torch.split(advantages, tuple(lengths.flatten()))
+        # advantages_split=torch.split(advantages, tuple(lengths.flatten()))
 
-        reinforce_loss = -logprobs.squeeze() * advantages.squeeze()
+        reinforce_loss = -logprobs.squeeze(dim=1) * advantages
         vf_loss = self.MSE_loss(values.squeeze(), rewards.squeeze())
         loss = reinforce_loss + vf_loss
-
         loss_per_step = torch.split(loss, tuple(lengths.flatten()))
-        loss_per_episode=torch.stack([torch.sum(l) for l in loss_per_step])
-        # self.writer.add_scalar('reinforce_loss', reinforce_loss.mean(), self.writer_iteration + 1)
-        # self.writer.add_scalar('vf_loss', vf_loss.mean(), self.writer_iteration + 1)
-        # self.writer.add_scalar('loss', loss.mean(), self.writer_iteration + 1)
+
+
+        loss_per_episode = torch.stack([torch.sum(l) for l in loss_per_step])
+        self.writer.add_scalar('reinforce_loss', reinforce_loss.mean(), self.writer_iteration + 1)
+        self.writer.add_scalar('vf_loss', vf_loss.mean(), self.writer_iteration + 1)
+        self.writer.add_scalar('loss', loss_per_episode.mean(), self.writer_iteration + 1)
         # loss=torch.cat(losses)
         self.writer_iteration += 1
 
