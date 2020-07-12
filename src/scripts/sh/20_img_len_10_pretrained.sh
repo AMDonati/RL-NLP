d0122733ba65cdbd367766075c75f7095b859fc5
Guillaume Quispe <guillaume.quispe@telecom-paristech.fr>
2020-07-13 01:40:39 +0200

    sh

--- src/scripts/sh/20_img_len_10_pretrained.sh
+++ src/scripts/sh/20_img_len_10_pretrained.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-python src/scripts/run.py -max_len 10 -data_path "data" -out_path "output/RL/20_img_len_10_pretrained" -model "lstm" -update_every 20 -agent "PPO" -K_epochs 10 -eps_clip 0.02 -word_emb_size 8 -hidden_size 24 -num_episodes_train 50000 -policy_path "output/RL/20_img_len_10_pretrained/experiments/pretrain/20200711-131316/model.pt"
+python src/scripts/run.py -max_len 10 -data_path "data" -out_path "output/RL/20_img_len_10_pretrained" -model "lstm" -update_every 20 -agent "PPO" -K_epochs 10 -eps_clip 0.02 -word_emb_size 8 -hidden_size 24 -num_episodes_train 50000 -policy_path "output/RL/20_img_len_10_pretrained/experiments/pretrain/20200711-190549/model.pth" -debug "50000,50020"
