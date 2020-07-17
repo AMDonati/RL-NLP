565db335a96423709cceebc6f473e4fb3b2aa249
Guillaume Quispe <guillaume.quispe@telecom-paristech.fr>
2020-07-17 21:53:49 +0200

    merge to one file

--- src/agent/reinforce.py
+++ src/agent/reinforce.py
@@ -1,4 +1,3 @@
-import numpy as np
 import torch
 import torch.nn as nn
 from torch.nn.utils.rnn import pad_sequence
@@ -10,83 +9,63 @@ class REINFORCE(Agent):
     def __init__(self, policy, env, test_envs, writer, out_path, gamma=1., lr=1e-2, grad_clip=None, pretrained_lm=None,
                  lm_sl=True,
                  pretrain=False, update_every=50, num_truncated=10, log_interval=10):
-
-        Agent.__init__(self, policy, env, writer=writer, out_path=out_path,
-                       gamma=gamma, lr=lr, grad_clip=grad_clip,
+        Agent.__init__(self, policy, env, writer, out_path, gamma=gamma, lr=lr, grad_clip=grad_clip,
                        pretrained_lm=pretrained_lm,
                        lm_sl=lm_sl,
                        pretrain=pretrain, update_every=update_every,
                        num_truncated=num_truncated,
                        log_interval=log_interval, test_envs=test_envs)
-        self.update_every = update_every
-        self.grad_clip = grad_clip
         self.MSE_loss = nn.MSELoss(reduction="none")
+        self.grad_clip = grad_clip
         self.update_mode = "episode"
         self.writer_iteration = 0
 
     def select_action(self, state, num_truncated=10, forced=None):
-        valid_actions, actions_probs = self.get_top_k_words(state.text, num_truncated)
+        valid_actions, actions_probs = self.get_top_k_words(state.text, num_truncated, state.img)
         policy_dist, policy_dist_truncated, value = self.policy(state.text, state.img, valid_actions)
-        action = policy_dist_truncated.sample() if forced is None else forced
+        action = policy_dist.sample() if forced is None else forced
         if policy_dist_truncated.probs.size() != policy_dist.probs.size():
             action = torch.gather(valid_actions, 1, action.view(1, 1))
         log_prob = policy_dist.log_prob(action.to(self.device)).view(-1)
         return action, log_prob, value, (valid_actions, actions_probs), policy_dist
 
+    def evaluate(self, state_text, state_img, action, num_truncated=10):
+        valid_actions, actions_probs = self.get_top_k_words(state_text, num_truncated)
+        policy_dist, policy_dist_truncated, value = self.policy(state_text, state_img, valid_actions)
+        dist_entropy = policy_dist.entropy()
+        log_prob = policy_dist.log_prob(action.view(-1))
+
+        return log_prob, value, dist_entropy
+
     def update(self):
-        # getting the lengths for the loss split
-        is_terminals = np.array(self.memory.is_terminals)
-        lengths = np.argwhere(is_terminals == True) + 1
-        lengths[1:] = lengths[1:] - lengths[:-1]
 
         rewards = []
         discounted_reward = 0
         for reward, is_terminal in zip(reversed(self.memory.rewards), reversed(self.memory.is_terminals)):
-            discounted_reward = reward + (self.gamma * discounted_reward * (1 - is_terminal))
+            if is_terminal:
+                discounted_reward = 0
+            discounted_reward = reward + (self.gamma * discounted_reward)
             rewards.insert(0, discounted_reward)
+        rewards = torch.tensor(rewards).to(self.device).float()
 
-        rewards = torch.tensor(rewards).to(self.device).float().view(-1, 1)
-        logprobs = torch.stack(self.memory.logprobs).to(self.device)
-        values = torch.stack(self.memory.values)
-        advantages = rewards.squeeze() - values.squeeze().detach()
-        # advantages_split=torch.split(advantages, tuple(lengths.flatten()))
+        old_logprobs = torch.stack(self.memory.logprobs).to(self.device)
+        old_values = torch.stack(self.memory.values).to(self.device)
 
-        states_text = pad_sequence(self.memory.states_text, batch_first=True, padding_value=0).to(self.device)
-
-        policy_dist, policy_dist_truncated, values = self.policy(states_text, torch.stack(self.memory.states_img))
-        probs = torch.gather(policy_dist.probs, 1, torch.stack(self.memory.actions))
-        logprobs = torch.log(probs)
-
-        reinforce_loss = -logprobs.squeeze(dim=1) * advantages
-        vf_loss = self.MSE_loss(values.squeeze(), rewards.squeeze())
+        advantages = rewards - old_values.detach().squeeze() if not self.pretrain else 1
+        reinforce_loss = -old_logprobs.view(-1)*advantages
+        vf_loss = 0.5 * self.MSE_loss(old_values.view(-1), rewards) if not self.pretrain else torch.tensor(
+            [0]).float().to(
+            self.device)
         loss = reinforce_loss + vf_loss
-        loss_per_step = torch.split(loss, tuple(lengths.flatten()))
-
-        loss_per_episode = torch.stack([torch.sum(l) for l in loss_per_step])
-        self.writer.add_scalar('reinforce_loss', reinforce_loss.mean(), self.writer_iteration + 1)
-        self.writer.add_scalar('vf_loss', vf_loss.mean(), self.writer_iteration + 1)
-        self.writer.add_scalar('loss', loss_per_episode.mean(), self.writer_iteration + 1)
-        # loss=torch.cat(losses)
-        self.writer_iteration += 1
 
         # take gradient step
         self.optimizer.zero_grad()
-        loss_per_episode.mean().backward()
+        loss.sum().backward()
         # clip grad norm:
-        if self.grad_clip is not None:
-            torch.nn.utils.clip_grad_norm_(parameters=self.policy.parameters(), max_norm=self.grad_clip)
         self.optimizer.step()
+        # compute grad norm:
+        self.writer_iteration += 1
 
-        # debug
-        # states_text = pad_sequence(self.memory.states_text, batch_first=True, padding_value=0).to(self.device)
+        return loss.mean()
 
-        policy_dist, policy_dist_truncated, value = self.policy(states_text, torch.stack(self.memory.states_img))
-        new_probs = torch.gather(policy_dist.probs, 1, torch.stack(self.memory.actions))
-        diff = torch.log(new_probs) - logprobs
-        # this must be positive
-        diff_adv = diff.squeeze() * advantages
 
-        #if torch.sum(diff_adv < 0).numpy() > 0:
-            #print(diff)
-            #print(diff_adv)
-        return loss_per_episode.mean()
