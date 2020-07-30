6a0d1fdfa01a4940fc10f8e0946f37252e33668e
AMDonati <alice.martindonati@gmail.com>
2020-07-30 11:48:08 +0200

    sh script eval

---
+++ src/scripts/sh/EVAL_ppo_20000_img_len_20.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+python src/scripts/run.py -max_len 20 -data_path "data" -out_path "output/RL/20000_img_len_20" -model "lstm" -update_every 128 -agent "PPO" -K_epochs 20 -eps_clip 0.02 -lr 0.001 -word_emb_size 32 -hidden_size 64 -num_episodes_train 0 -num_truncated 87 -debug "0,20000" -grad_clip 1 -num_questions 8 -lm_path "output/lm_model/model.pt" -policy_path "output/RL/20000_img_len_20/experiments/train/scratch/model.pth" -num_episodes_test 200
+
+python src/scripts/run.py -max_len 20 -data_path "data" -out_path "output/RL/20000_img_len_20" -model "lstm" -update_every 128 -agent "PPO" -K_epochs 20 -eps_clip 0.02 -lr 0.001 -word_emb_size 32 -hidden_size 64 -num_episodes_train 0 -num_truncated 87 -debug "0,20000" -num_questions 8 -lm_path "output/lm_model/model.pt" -policy_path "output/RL/20000_img_len_20/experiments/train/pretrain/model.pth" -num_episodes_test 200
+
+python src/scripts/run.py -max_len 20 -data_path "data" -out_path "output/RL/20000_img_len_20" -model "lstm" -update_every 128 -agent "PPO" -K_epochs 20 -eps_clip 0.02 -lr 0.001 -word_emb_size 32 -hidden_size 64 -num_episodes_train 0 -num_truncated 10 -debug "0,20000" -num_questions 8 -grad_clip 1 -lm_path "output/lm_model/model.pt" -policy_path "output/RL/20000_img_len_20/experiments/train/top_k10/model.pth" -num_episodes_test 200
+
+python src/scripts/run.py -max_len 20 -data_path "data" -out_path "output/RL/20000_img_len_20" -model "lstm" -update_every 128 -agent "PPO" -K_epochs 20 -eps_clip 0.02 -lr 0.001 -word_emb_size 32 -hidden_size 64 -num_episodes_train 0 -num_truncated 20 -debug "0,20000" -num_questions 8 -grad_clip 1 -lm_path "output/lm_model/model.pt" -policy_path "output/RL/20000_img_len_20/experiments/train/top_k20/model.pth" -num_episodes_test 200
