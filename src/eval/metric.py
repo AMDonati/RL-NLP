4c7179c40fdba233856dddc0451cad3a3c440e6b
AMDonati <alice.martindonati@gmail.com>
2020-07-31 18:52:49 +0200

    ignoring no tokens in DialogMetric (solve false bug in length 20 for pretrain dialog

--- src/eval/metric.py
+++ src/eval/metric.py
@@ -183,7 +183,7 @@ class DialogMetric(Metric):
                 self.generated_dialog[self.train_test + '_' + self.key] = [kwargs["state"].text[:, 1:].squeeze().cpu()]
             else:
                 self.generated_dialog[self.train_test + '_' + self.key].append(kwargs["state"].text[:, 1:].squeeze().cpu())
-            state_decoded = self.agent.env.clevr_dataset.idx2word(kwargs["state"].text[:, 1:].numpy()[0])
+            state_decoded = self.agent.env.clevr_dataset.idx2word(kwargs["state"].text[:, 1:].numpy()[0], ignored=[])
             closest_question_decoded = kwargs["closest_question"]
             string = ' img {}:'.format(kwargs["img_idx"]) + state_decoded + '\n' + 'CLOSEST QUESTION:' + closest_question_decoded + '\n' + '-'*40
             self.metric.append(string)
