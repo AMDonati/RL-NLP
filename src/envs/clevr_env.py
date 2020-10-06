e790bef18b9034c05f40b871fb6faf288bd5f78c
Guillaume Quispe <guillaume.quispe@telecom-paristech.fr>
2020-10-06 18:02:07 +0200

    answer reward

--- src/envs/clevr_env.py
+++ src/envs/clevr_env.py
@@ -98,11 +98,11 @@ class ClevrEnv(gym.Env):
         self.ref_question_idx = random.choice(range(self.ref_questions.size(0)))
 
         self.ref_question = self.ref_questions[self.ref_question_idx]
+        self.ref_answer = self.ref_answers[self.ref_question_idx]
         if self.condition_answer != "none":
             self.ref_questions = self.ref_questions[self.ref_question_idx:self.ref_question_idx + 1]
             self.ref_answers = self.ref_answers[self.ref_question_idx:self.ref_question_idx + 1]
 
-        self.ref_answer = self.ref_answers[0]
         self.ref_questions_decoded = [self.clevr_dataset.idx2word(question, ignored=['<SOS>', '<PAD>'])
                                       for question in self.ref_questions.numpy()]
 
