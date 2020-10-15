59e134dcdf52e8a1d54a9345dc27274a16388b21
Guillaume Quispe <guillaume.quispe@telecom-paristech.fr>
2020-10-15 19:00:41 +0200

    language model classes

--- src/models/language_model.py
+++ src/models/language_model.py
@@ -1,8 +1,5 @@
 import torch
 import torch.nn.functional as F
-from transformers import AutoTokenizer
-
-from models.LM_networks import LSTMModel
 
 
 class LanguageModel:
@@ -14,8 +11,9 @@ class LanguageModel:
     def forward(self, state):
         pass
 
+
 class ClevrLanguageModel(LanguageModel):
-    def __init__(self,pretrained_lm, clevr_dataset, tokenizer=None):
+    def __init__(self, pretrained_lm, clevr_dataset, tokenizer=None):
         LanguageModel.__init__(self, pretrained_lm, clevr_dataset)
 
     def forward(self, state_text):
@@ -27,6 +25,7 @@ class ClevrLanguageModel(LanguageModel):
         log_probas = log_probas[:, -1, :]
         return log_probas, logits
 
+
 class GenericLanguageModel(LanguageModel):
     def __init__(self, pretrained_lm, clevr_dataset, tokenizer=None):
         LanguageModel.__init__(self, pretrained_lm, clevr_dataset)
@@ -34,15 +33,13 @@ class GenericLanguageModel(LanguageModel):
         self.clevr_to_lm_trad = {value: self.tokenizer.encode(" " + key)[0] for
                                  key, value in self.dataset.vocab_questions.items() if
                                  len(self.tokenizer.encode(" " + key)) == 1}
+
     def forward(self, state_text):
         text = "bos " + self.dataset.idx2word(state_text.cpu().numpy().ravel(), stop_at_end=True)
         input_ids = self.tokenizer.encode(text, return_tensors="pt")
         logits_lm = self.language_model(input_ids)[0][:, -1, :]
         logits = -torch.ones(len(self.dataset.vocab_questions)) * 1e32
         logits[list(self.clevr_to_lm_trad.keys())] = logits_lm[0][list(self.clevr_to_lm_trad.values())]
-        # filtered_next_token_logits = top_k_top_p_filtering(next_token_logits, top_k=50, top_p=1.0)
-        # probs = F.softmax(filtered_next_token_logits, dim=-1)
-        # next_token = torch.multinomial(probs, num_samples=1)
         logits = logits.unsqueeze(dim=0)
         log_probas = F.log_softmax(logits, dim=-1)
         return log_probas, logits
