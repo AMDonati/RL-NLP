ebdbc34f7e42c91adaff65dfc28b1933949f1c7c
AMDonati <alice.martindonati@gmail.com>
2020-10-20 14:12:03 +0200

    sh

--- sh/extract_features.sh
+++ sh/extract_features.sh
@@ -1,11 +1,4 @@
-python src/preprocessing/extract_features.py \
-  --input_image_dir data/CLEVR_v1.0/images/train \
-  --output_h5_file data/train_features.h5 --batch_size 16
-
-python src/preprocessing/extract_features.py \
-  --input_image_dir data/CLEVR_v1.0/images/val \
-  --output_h5_file data/val_features.h5 --batch_size 16
-
-python src/preprocessing/extract_features.py \
-  --input_image_dir data/CLEVR_v1.0/images/test \
-  --output_h5_file data/test_features.h5 --batch_size 16
+#!/usr/bin/env bash
+python src/preprocessing/extract_features.py --input_image_dir data/CLEVR_v1.0/images/train --output_h5_file data/train_features.h5 --batch_size 128
+python src/preprocessing/extract_features.py --input_image_dir data/CLEVR_v1.0/images/val --output_h5_file data/val_features.h5 --batch_size 128
+python src/preprocessing/extract_features.py --input_image_dir data/CLEVR_v1.0/images/test --output_h5_file data/test_features.h5 --batch_size 128
--- sh/preprocess_questions.sh
+++ sh/preprocess_questions.sh
@@ -1,22 +1,7 @@
+#!/usr/bin/env bash
 # execute this command lines from the source github folder.
 # First, execute the following command line: export PYTHONPATH=src:${PYTHONPATH}
-
-python src/preprocessing/preprocess_questions.py -data_path "data/CLEVR_v1.0/questions/CLEVR_train_questions.json" \
--out_vocab_path "data/vocab.json" -out_h5_path "data/train_questions.h5" -min_token_count 1
-
-python src/preprocessing/preprocess_questions.py -data_path "data/CLEVR_v1.0/questions/CLEVR_val_questions.json" \
--out_vocab_path "data/vocab.json" -out_h5_path "data/val_questions.h5" -min_token_count 1
-
-python src/preprocessing/preprocess_questions.py -data_path "data/CLEVR_v1.0/questions/CLEVR_test_questions.json" \
--out_vocab_path "data/vocab.json" -out_h5_path "data/test_questions.h5" -min_token_count 1
-
-
-
-# to extract a smaller json with a subset of the datasets.
-python src/preprocessing/extract_questions_subset.py -data_path "data/CLEVR_v1.0/questions/train_questions.json"
--out_path "data/CLEVR_v1.0/temp/50000_20000_samples/train_questions.json"
-
-
-# set of starting words when building vocab:
-set of starting words...: ['The', 'Does', 'There', 'What', 'How', 'Are', 'Is', 'Do']
+python src/preprocessing/preprocess_questions.py -data_path "data/CLEVR_v1.0/questions/CLEVR_train_questions.json" -out_vocab_path "data/vocab.json" -out_h5_path "data/train_questions.h5" -min_token_count 1
+python src/preprocessing/preprocess_questions.py -data_path "data/CLEVR_v1.0/questions/CLEVR_val_questions.json" -out_vocab_path "data/vocab.json" -out_h5_path "data/val_questions.h5" -min_token_count 1
+python src/preprocessing/preprocess_questions.py -data_path "data/CLEVR_v1.0/questions/CLEVR_test_questions.json" -out_vocab_path "data/vocab.json" -out_h5_path "data/test_questions.h5" -min_token_count 1
 
