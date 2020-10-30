cdb6a779ffaec6ddfc5f8882004a8daaaad53677
Guillaume Quispe <guillaume.quispe@telecom-paristech.fr>
2020-10-30 12:35:16 +0100

    environment done if question mark

--- src/envs/clevr_env.py
+++ src/envs/clevr_env.py
@@ -55,7 +55,7 @@ class ClevrEnv(gym.Env):
 
     def check_if_done(self, action):
         done = False
-        is_action_terminal = action.item() in [self.special_tokens.EOS_idx]
+        is_action_terminal = action.item() in [self.special_tokens.EOS_idx, self.special_tokens.question_mark_idx]
         if is_action_terminal or self.step_idx == (self.max_len - 1):
             done = True
         return done
