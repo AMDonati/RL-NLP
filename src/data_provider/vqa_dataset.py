70c3fb019d8c9d197851d2e2c71db65eb56ac03f
AMDonati <alice.martindonati@gmail.com>
2020-11-24 21:17:01 +0100

    correction of split_function by removing assert function

--- src/data_provider/vqa_dataset.py
+++ src/data_provider/vqa_dataset.py
@@ -48,7 +48,6 @@ def split_question(question):
     last_id = question.nonzero()[-1]
     input_question = torch.cat([question[:last_id], question[last_id+1:]])
     target_question = question[1:]
-    #assert_correctness_batch(input_question[:last_id], target_question[:last_id])
     return input_question, target_question
 
 
@@ -481,9 +480,7 @@ class VQADataset(Dataset):
         if add_sos_token:
             question = torch.cat([torch.tensor(self.vocab_questions["<SOS>"]).view(1), question])
 
-        #inputs, targets = split_question(question)
-        inputs = question[:-1]
-        targets = question[1:]
+        inputs, targets = split_question(question)
 
         return (inputs, targets), labels, (features, image_mask, spatials)
 
