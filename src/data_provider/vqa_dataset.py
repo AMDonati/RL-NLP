c5483935c39ec9bd30ca8eb3b811d1df467b9428
AMDonati <alice.martindonati@gmail.com>
2020-11-25 20:10:51 +0100

    back to old split input/tar

--- src/data_provider/vqa_dataset.py
+++ src/data_provider/vqa_dataset.py
@@ -480,7 +480,9 @@ class VQADataset(Dataset):
         if add_sos_token:
             question = torch.cat([torch.tensor(self.vocab_questions["<SOS>"]).view(1), question])
 
-        inputs, targets = split_question(question)
+        inputs = question[:-1]
+        targets = question[1:]
+        #inputs, targets = split_question(question)
 
         return (inputs, targets), labels, (features, image_mask, spatials)
 
