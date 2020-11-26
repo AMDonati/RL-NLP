e04a293e06707c6e40a75862ea4f13ae5a5c64e0
Guillaume Quispe <guillaume.quispe@telecom-paristech.fr>
2020-11-26 18:48:48 +0100

    debug_script.sh

---
+++ src/scripts/sh/debug/debug_script.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+FILENAME=output/RL/test_before_merge_$(date +"%Y_%m_%d_%I_%M_%p").txt
+src/scripts/sh/debug/debug_script_vqav2.sh   > $FILENAME 2>&1
+src/scripts/sh/debug/debug_script_clevr.sh   > $FILENAME 2>&1
+src/scripts/sh/debug/debug_script_train.sh   > $FILENAME 2>&1
