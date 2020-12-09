eff40de5f2b6898b0cdf40f0ee6e5951e73e9f38
AMDonati <alice.martindonati@gmail.com>
2020-12-09 08:42:07 +0100

    sh

--- src/scripts/sh/sl/sl_vqa.sh
+++ src/scripts/sh/sl/sl_vqa.sh
@@ -3,7 +3,7 @@ python src/train/launch_train.py -task "policy" -dataset "vqa" -data_path "data/
 
 python src/train/launch_train.py -task "policy" -dataset "vqa" -data_path "data/vqa-v2" -features_path "data/vqa-v2/coco_trainval.lmdb" -out_path "output/vqa_policy_smallvocab" -emb_size 512 -hidden_size 1024 -bs 512 -ep 20 -num_workers 6 -fusion "average" -condition_answer "after_fusion" -min_data 1 -device_id 1
 
-python src/train/launch_train.py -task "lm" -dataset "vqa" -model "lstm" -num_layers 1 -emb_size 512 -hidden_size 512 -p_drop 0.1 -lr 0.001 -data_path "data/vqa-v2" -features_path "data/vqa-v2/coco_trainval.lmdb" -out_path "output/vqa_lm_model_newvocab" -bs 512 -ep 20 -num_workers 6 -data_min 0 -device_id 2
+python src/train/launch_train.py -task "lm" -dataset "vqa" -model "lstm" -num_layers 1 -emb_size 512 -hidden_size 512 -p_drop 0.1 -lr 0.001 -data_path "data/vqa-v2" -features_path "data/vqa-v2/coco_trainval.lmdb" -out_path "output/vqa_lm_model_newvocab" -bs 512 -ep 20 -num_workers 6 -min_data 0 -device_id 2
 
 python src/train/launch_train.py -task "lm" -dataset "vqa" -model "lstm" -num_layers 1 -emb_size 512 -hidden_size 512 -p_drop 0.1 -lr 0.001 -data_path "data/vqa-v2" -features_path "data/vqa-v2/coco_trainval.lmdb" -out_path "output/vqa_lm_model_small_vocab" -bs 512 -ep 50 -num_workers 6 -min_data 1 -device_id 3
 
