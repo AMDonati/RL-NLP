176a049d8bbc0740ad3d1d5f3809d9cbb6f712f9
gqkc <guillaume.quispe@gmail.com>
2020-12-09 12:26:16 +0100

    dialogimage

--- src/scripts/run.py
+++ src/scripts/run.py
@@ -133,7 +133,7 @@ def get_parser():
                                  "ttr_question", "sum_probs", "true_word_rank", "true_word_prob"], help="train metrics")
     parser.add_argument('-test_metrics', nargs='+', type=str,
                         default=["return", "dialog", "bleu", "ppl_dialog_lm",
-                                 "ttr_question", "sum_probs", "ppl", "lv_norm", "ttr"],
+                                 "ttr_question", "sum_probs", "ppl", "lv_norm", "ttr", "dialogimage"],
                         help="test metrics")
     parser.add_argument('-test_modes', nargs='+', type=str,
                         default=["test_images"],
