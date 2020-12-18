0d66ca37acdda0ef38c28ec0646e957d8600ccd6
gqkc <guillaume.quispe@gmail.com>
2020-12-18 22:51:24 +0100

    lr

--- src/scripts/sh/vqav2_bleu/no_answer/vqav2_lv_128_256.sh
+++ src/scripts/sh/vqav2_bleu/no_answer/vqav2_lv_128_256.sh
@@ -17,15 +17,15 @@ NUM_EPISODE_TEST=10
 ENV_="vqa"
 MODEL="lstm"
 AGENT="PPO"
-LR=0.001
+LR=0.0002
 WORD_EMB_SIZE=128
 HIDDEN_SIZE=256
-EPS_CLIP=0.00001
+EPS_CLIP=0.2
 REWARD="lv_norm"
 FUSION="average"
 CONDITION_ANSWER="none"
 MIN_DATA=1
-DEBUG="0,1"
+DEBUG="0,4"
 NUM_QUESTIONS=1
 
 if [ -n "$1" ]; then
@@ -35,13 +35,12 @@ fi
 echo "$(date +"%Y_%m_%d_%I_%M_%p")-------------------------  top p ---------------------------------------------------------------------------------------------------------------"
 python src/scripts/run.py -env $ENV_ -max_len $MAX_LEN -data_path $DATA_PATH -out_path $OUTPUT_PATH -model $MODEL -update_every $UPDATE_EVERY -agent $AGENT -K_epochs $K_EPOCHS -eps_clip $EPS_CLIP -lr $LR -word_emb_size $WORD_EMB_SIZE -hidden_size $HIDDEN_SIZE -num_episodes_train $NUM_EPISODE_TRAIN -lm_path $LM_PATH -reward $REWARD -num_episodes_test $NUM_EPISODE_TEST -mask_answers 1 -grad_clip 1 -fusion $FUSION -condition_answer $CONDITION_ANSWER -features_path $FEATURES_PATH -debug $DEBUG -min_data $MIN_DATA -num_questions $NUM_QUESTIONS -truncate_mode "top_p" -top_p 0.96
 
-DEBUG="0,2"
+DEBUG="0,10"
 NUM_EPISODE_TRAIN=7000
-echo "$(date +"%Y_%m_%d_%I_%M_%p")-------------------------  top p 2 images ---------------------------------------------------------------------------------------------------------------"
+echo "$(date +"%Y_%m_%d_%I_%M_%p")-------------------------  top p  ---------------------------------------------------------------------------------------------------------------"
 python src/scripts/run.py -env $ENV_ -max_len $MAX_LEN -data_path $DATA_PATH -out_path $OUTPUT_PATH -model $MODEL -update_every $UPDATE_EVERY -agent $AGENT -K_epochs $K_EPOCHS -eps_clip $EPS_CLIP -lr $LR -word_emb_size $WORD_EMB_SIZE -hidden_size $HIDDEN_SIZE -num_episodes_train $NUM_EPISODE_TRAIN -lm_path $LM_PATH -reward $REWARD -num_episodes_test $NUM_EPISODE_TEST -mask_answers 1 -grad_clip 1 -fusion $FUSION -condition_answer $CONDITION_ANSWER -features_path $FEATURES_PATH -debug $DEBUG -min_data $MIN_DATA -num_questions $NUM_QUESTIONS -truncate_mode "top_p" -top_p 0.96
 
-DEBUG="0,3"
+DEBUG="0,20"
 NUM_EPISODE_TRAIN=10000
-
-echo "$(date +"%Y_%m_%d_%I_%M_%p")-------------------------  top p 2 images ---------------------------------------------------------------------------------------------------------------"
+echo "$(date +"%Y_%m_%d_%I_%M_%p")-------------------------  top p eps ---------------------------------------------------------------------------------------------------------------"
 python src/scripts/run.py -env $ENV_ -max_len $MAX_LEN -data_path $DATA_PATH -out_path $OUTPUT_PATH -model $MODEL -update_every $UPDATE_EVERY -agent $AGENT -K_epochs $K_EPOCHS -eps_clip $EPS_CLIP -lr $LR -word_emb_size $WORD_EMB_SIZE -hidden_size $HIDDEN_SIZE -num_episodes_train $NUM_EPISODE_TRAIN -lm_path $LM_PATH -reward $REWARD -num_episodes_test $NUM_EPISODE_TEST -mask_answers 1 -grad_clip 1 -fusion $FUSION -condition_answer $CONDITION_ANSWER -features_path $FEATURES_PATH -debug $DEBUG -min_data $MIN_DATA -num_questions $NUM_QUESTIONS -truncate_mode "top_p" -top_p 0.96
