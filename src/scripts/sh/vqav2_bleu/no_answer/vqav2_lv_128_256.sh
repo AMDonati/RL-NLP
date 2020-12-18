65061f94d9f2af64f624b4a1713b56cbe8928556
gqkc <guillaume.quispe@gmail.com>
2020-12-18 20:28:40 +0100

    hidden size

--- src/scripts/sh/vqav2_bleu/no_answer/vqav2_lv_128_256.sh
+++ src/scripts/sh/vqav2_bleu/no_answer/vqav2_lv_128_256.sh
@@ -12,7 +12,7 @@ POLICY_PATH="output/VQAv2_SL_32_64/model.pt"
 K_EPOCHS=20
 MAX_LEN=5
 UPDATE_EVERY=512
-NUM_EPISODE_TRAIN=3000
+NUM_EPISODE_TRAIN=5000
 NUM_EPISODE_TEST=10
 ENV_="vqa"
 MODEL="lstm"
@@ -36,12 +36,12 @@ echo "$(date +"%Y_%m_%d_%I_%M_%p")-------------------------  top p -------------
 python src/scripts/run.py -env $ENV_ -max_len $MAX_LEN -data_path $DATA_PATH -out_path $OUTPUT_PATH -model $MODEL -update_every $UPDATE_EVERY -agent $AGENT -K_epochs $K_EPOCHS -eps_clip $EPS_CLIP -lr $LR -word_emb_size $WORD_EMB_SIZE -hidden_size $HIDDEN_SIZE -num_episodes_train $NUM_EPISODE_TRAIN -lm_path $LM_PATH -reward $REWARD -num_episodes_test $NUM_EPISODE_TEST -mask_answers 1 -grad_clip 1 -fusion $FUSION -condition_answer $CONDITION_ANSWER -features_path $FEATURES_PATH -debug $DEBUG -min_data $MIN_DATA -num_questions $NUM_QUESTIONS -truncate_mode "top_p" -top_p 0.96
 
 DEBUG="0,2"
-NUM_EPISODE_TRAIN=5000
+NUM_EPISODE_TRAIN=7000
 echo "$(date +"%Y_%m_%d_%I_%M_%p")-------------------------  top p 2 images ---------------------------------------------------------------------------------------------------------------"
 python src/scripts/run.py -env $ENV_ -max_len $MAX_LEN -data_path $DATA_PATH -out_path $OUTPUT_PATH -model $MODEL -update_every $UPDATE_EVERY -agent $AGENT -K_epochs $K_EPOCHS -eps_clip $EPS_CLIP -lr $LR -word_emb_size $WORD_EMB_SIZE -hidden_size $HIDDEN_SIZE -num_episodes_train $NUM_EPISODE_TRAIN -lm_path $LM_PATH -reward $REWARD -num_episodes_test $NUM_EPISODE_TEST -mask_answers 1 -grad_clip 1 -fusion $FUSION -condition_answer $CONDITION_ANSWER -features_path $FEATURES_PATH -debug $DEBUG -min_data $MIN_DATA -num_questions $NUM_QUESTIONS -truncate_mode "top_p" -top_p 0.96
 
 DEBUG="0,3"
-NUM_EPISODE_TRAIN=7000
+NUM_EPISODE_TRAIN=10000
 
 echo "$(date +"%Y_%m_%d_%I_%M_%p")-------------------------  top p 2 images ---------------------------------------------------------------------------------------------------------------"
 python src/scripts/run.py -env $ENV_ -max_len $MAX_LEN -data_path $DATA_PATH -out_path $OUTPUT_PATH -model $MODEL -update_every $UPDATE_EVERY -agent $AGENT -K_epochs $K_EPOCHS -eps_clip $EPS_CLIP -lr $LR -word_emb_size $WORD_EMB_SIZE -hidden_size $HIDDEN_SIZE -num_episodes_train $NUM_EPISODE_TRAIN -lm_path $LM_PATH -reward $REWARD -num_episodes_test $NUM_EPISODE_TEST -mask_answers 1 -grad_clip 1 -fusion $FUSION -condition_answer $CONDITION_ANSWER -features_path $FEATURES_PATH -debug $DEBUG -min_data $MIN_DATA -num_questions $NUM_QUESTIONS -truncate_mode "top_p" -top_p 0.96
