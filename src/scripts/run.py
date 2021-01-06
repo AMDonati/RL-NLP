9a40fe5cfc1083d353914d01e4ee9094eae1e5bc
gqkc <guillaume.quispe@gmail.com>
2021-01-06 18:12:30 +0100

    numtruncated top p

--- src/scripts/run.py
+++ src/scripts/run.py
@@ -149,7 +149,7 @@ def get_parser():
     parser.add_argument('-train_metrics', nargs='+', type=str,
                         default=["return", "size_valid_actions",
                                  "valid_actions", "dialog", "eps_truncation",
-                                 "ttr_question", "sum_probs", "true_word_rank", "true_word_prob", "dialogimage"],
+                                 "ttr", "sum_probs", "true_word_rank", "true_word_prob", "dialogimage"],
                         help="train metrics")
     parser.add_argument('-test_metrics', nargs='+', type=str,
                         default=["return", "dialog", "bleu", "ppl_dialog_lm",
