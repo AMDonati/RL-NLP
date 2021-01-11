b014f9cfdee52f5e2cac8cedda723dd45305c8e2
gqkc <guillaume.quispe@gmail.com>
2021-01-11 15:06:58 +0100

    get preds

--- src/RL_toolbox/reward.py
+++ src/RL_toolbox/reward.py
@@ -271,7 +271,7 @@ class VILBERT(Reward):
         if not done:
             return 0, "N/A", None
         #try:
-        vil_prediction, target = self.get_preds(question,ep_questions_decoded)
+        vil_prediction, target = self.get_preds(question)
         if self.reduced_answers:
             mask = torch.ones_like(vil_prediction) * float("-Inf")
             mask[:, self.dataset.reduced_answers.squeeze()] = vil_prediction[:,
