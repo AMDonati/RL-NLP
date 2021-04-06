42e2a78f88f6d5216ff022b98717aeb411746786
Alice Martin <alice.martindonati@gmail.com>
2021-04-06 15:14:29 +0200

    SLURM script CLEVR extract features

---
+++ src/scripts/sh/slurm/clevr_features.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SBATCH --job-name=clevrfeatures
+#SBATCH --qos=qos_gpu-t3
+#SBATCH --nodes=1
+#SBATCH --ntasks=16
+#SBATCH --gres=gpu:1
+#SBATCH --output=clevrfeatures%j.out
+#SBATCH --error=clevrfeatures%j.err
+#SBATCH --time=03:00:00
+
+module purge
+module load  pytorch-gpu/py3/1.7.1
+conda activate rl-nlp
+
+set -x
+srun python -u src/preprocessing/extract_features.py --input_image_dir data/CLEVR_v1.0/images/train --output_h5_file data/train_features.h5 --batch_size 64
+srun python -u src/preprocessing/extract_features.py --input_image_dir data/CLEVR_v1.0/images/val --output_h5_file data/val_features.h5 --batch_size 64
+srun python -u src/preprocessing/extract_features.py --input_image_dir data/CLEVR_v1.0/images/test --output_h5_file data/test_features.h5 --batch_size 64
