b91f90f4eb953a553efc7211a6c6880b7f8e9ef4
Alice Martin <alice.martindonati@gmail.com>
2021-04-07 15:28:34 +0200

    slurm script VQAv2 data

---
+++ src/scripts/sh/slurm/vqav2_data.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#SBATCH --job-name=clevrfeatures
+#SBATCH --qos=qos_gpu-t3
+#SBATCH -C v100-32g
+#SBATCH --nodes=1
+#SBATCH --ntasks=16
+#SBATCH --gres=gpu:4
+#SBATCH --cpus-per-task=4
+#SBATCH --output=clevrfeatures%j.out
+#SBATCH --error=clevrfeatures%j.err
+#SBATCH --time=03:00:00
+
+module purge
+module load  pytorch-gpu/py3/1.7.1
+conda activate rl-nlp
+
+export PYTHONPATH=src:${PYTHONPATH}
+
+set -x
+srun python -u src/preprocessing/preprocess_vqa_dataset.py -data_path "data/vqa-v2" -features_path "data/vqa-v2/coco_trainval.lmdb" -vocab_path "none" -min_split 0
+srun python -u src/preprocessing/preprocess_vqa_dataset.py -data_path "data/vqa-v2" -features_path "data/vqa-v2/coco_trainval.lmdb" -vocab_path "data/vqa-v2/cache/vocab.json" -split "train" -min_split 0 -test 1
+srun python -u src/preprocessing/preprocess_vqa_dataset.py -data_path "data/vqa-v2" -features_path "data/vqa-v2/coco_trainval.lmdb" -vocab_path "data/vqa-v2/cache/vocab.json" -split "val" -min_split 0 -test 1
