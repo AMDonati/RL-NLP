4fb30369984b018c28deee865fe0b98a8f259798
Alice Martin <alice.martindonati@gmail.com>
2021-04-08 15:01:21 +0200

    slurm scripts

--- src/scripts/sh/slurm/clevr_features.sh
+++ src/scripts/sh/slurm/clevr_features.sh
@@ -17,9 +17,9 @@ conda activate rl-nlp
 export PYTHONPATH=src:${PYTHONPATH}
 
 set -x
-#srun python -u src/preprocessing/preprocess_questions.py -data_path "data/CLEVR_v1.0/questions/CLEVR_train_questions.json" -out_vocab_path "data/vocab.json" -out_h5_path "data/train_questions.h5" -min_token_count 1
-#srun python -u src/preprocessing/preprocess_questions.py -data_path "data/CLEVR_v1.0/questions/CLEVR_val_questions.json" -out_vocab_path "data/vocab.json" -out_h5_path "data/val_questions.h5" -min_token_count 1
-#srun python -u src/preprocessing/preprocess_questions.py -data_path "data/CLEVR_v1.0/questions/CLEVR_test_questions.json" -out_vocab_path "data/vocab.json" -out_h5_path "data/test_questions.h5" -min_token_count 1
+srun python -u src/preprocessing/preprocess_questions.py -data_path "data/CLEVR_v1.0/questions/CLEVR_train_questions.json" -out_vocab_path "data/vocab.json" -out_h5_path "data/train_questions.h5" -min_token_count 1
+srun python -u src/preprocessing/preprocess_questions.py -data_path "data/CLEVR_v1.0/questions/CLEVR_val_questions.json" -out_vocab_path "data/vocab.json" -out_h5_path "data/val_questions.h5" -min_token_count 1
+srun python -u src/preprocessing/preprocess_questions.py -data_path "data/CLEVR_v1.0/questions/CLEVR_test_questions.json" -out_vocab_path "data/vocab.json" -out_h5_path "data/test_questions.h5" -min_token_count 1
 srun python -u src/preprocessing/extract_features.py --input_image_dir data/CLEVR_v1.0/images/train --output_h5_file data/train_features.h5 --batch_size 64
 srun python -u src/preprocessing/extract_features.py --input_image_dir data/CLEVR_v1.0/images/val --output_h5_file data/val_features.h5 --batch_size 64
 srun python -u src/preprocessing/extract_features.py --input_image_dir data/CLEVR_v1.0/images/test --output_h5_file data/test_features.h5 --batch_size 64
