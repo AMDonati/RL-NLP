0c8646225e7936ef3bdeb92c2c59293bf73c7f93
Alice Martin <alice.martindonati@gmail.com>
2021-04-23 15:06:35 +0200

    slurm

--- src/scripts/sh/slurm/vqav2/scratch.sh
+++ src/scripts/sh/slurm/vqav2/scratch.sh
@@ -17,11 +17,6 @@ conda activate rl-nlp-2
 
 export PYTHONPATH=src:${PYTHONPATH}
 
-
-echo "---------------------------------------------------------------------------------------------------------------------------------"
-echo "------------------------- VQAv2 -------------------------------------------------------------------------------------------------"
-echo "---------------------------------------------------------------------------------------------------------------------------------"
-
 DATA_PATH="data/vqa-v2/"
 FEATURES_PATH="data/vqa-v2/coco_trainval.lmdb/"
 LM_PATH="output/vqa_lm_model/model.pt"
@@ -48,4 +43,4 @@ REWARD="vilbert_rank2"
 FUSION="average"
 CONDITION_ANSWER="after_fusion"
 
-srun python -u src/scripts/run.py -env $ENV_ -max_len $MAX_LEN -data_path $DATA_PATH -out_path $OUTPUT_PATH -model $MODEL -update_every $UPDATE_EVERY -agent $AGENT -K_epochs $K_EPOCHS -eps_clip $EPS_CLIP -lr $LR -word_emb_size $WORD_EMB_SIZE -hidden_size $HIDDEN_SIZE -num_episodes_train $NUM_EPISODE_TRAIN -lm_path $LM_PATH -reward $REWARD -num_episodes_test $NUM_EPISODE_TEST -mask_answers 1 -grad_clip 1 -fusion $FUSION -condition_answer $CONDITION_ANSWER -features_path $FEATURES_PATH -reward_vocab $VILBERT_VOCAB -reward_path $VILBERT_PATH -eval_modes "greedy" "sampling
+srun python -u src/scripts/run.py -env $ENV_ -max_len $MAX_LEN -data_path $DATA_PATH -out_path $OUTPUT_PATH -model $MODEL -update_every $UPDATE_EVERY -agent $AGENT -K_epochs $K_EPOCHS -eps_clip $EPS_CLIP -lr $LR -word_emb_size $WORD_EMB_SIZE -hidden_size $HIDDEN_SIZE -num_episodes_train $NUM_EPISODE_TRAIN -lm_path $LM_PATH -reward $REWARD -num_episodes_test $NUM_EPISODE_TEST -mask_answers 1 -grad_clip 1 -fusion $FUSION -condition_answer $CONDITION_ANSWER -features_path $FEATURES_PATH -reward_vocab $VILBERT_VOCAB -reward_path $VILBERT_PATH -eval_modes "greedy" "sampling"
