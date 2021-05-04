d847346b25cbef10f2f7ca33b3cba29849f14837
gqkc <guillaume.quispe@gmail.com>
2021-05-04 09:04:25 +0200

    oracle score for vilbert

--- merge_metrics.py
+++ merge_metrics.py
@@ -1,5 +1,6 @@
 import argparse
 import os
+
 import pandas as pd
 
 
@@ -10,12 +11,34 @@ def get_parser():
     parser.add_argument('-columns_to_save', nargs='+', type=str,
                         default=['action_probs_truncated', 'bleu', 'lv_norm', 'ppl', 'ppl_dialog_lm',
                                  'return', 'selfbleu', 'size_valid_actions', 'sum_probs', 'ttr_question',
-                                 'vilbert', 'ttr'], help="")
+                                 'vilbert', 'ttr', "oracle_score"], help="")
     parser.add_argument('-bottom_folder', type=int, default=1)
     parser.add_argument('-top_folder', type=int, default=1)
     parser.add_argument('-precision', type=int, default=4)
 
     return parser
+
+
+def add_to_metrics(df, path_exp):
+    if "oracle_score" not in df.index:
+        means = {trunc: [] for trunc in list(df.columns)}
+        root, _, files = next(os.walk(os.path.join(path_exp, "metrics")))
+        for f in files:
+            if "train" not in f and "vilbert_recall_rewards" in f:
+                try:
+                    rew_df = pd.read_csv(os.path.join(root, f), header=None)
+                    mean_rew = rew_df[0].mean()
+                    if "no_trunc" in f:
+                        means["no_trunc"].append(mean_rew)
+                    if "with_trunc" in f:
+                        means["with_trunc"].append(mean_rew)
+                except pd.errors.EmptyDataError:
+                    print("empty file {}".format(os.path.join(root, f)))
+                    continue
+        oracle_score_serie = pd.DataFrame.from_dict(means).mean()
+        oracle_score_serie.name = "oracle_score"
+        df = df.append(oracle_score_serie)
+    return df
 
 
 def merge_one_experiment(args):
@@ -29,6 +52,7 @@ def merge_one_experiment(args):
             all_metrics_path = os.path.join(dir_experiment, "all_metrics.csv")
             if os.path.exists(all_metrics_path):
                 df_exp = pd.read_csv(all_metrics_path, index_col=0)
+                df_exp = add_to_metrics(df_exp, dir_experiment)
                 if "with_trunc" in df_exp.columns:
                     df_with_trunc = df_with_trunc.append(df_exp["with_trunc"].to_dict(), ignore_index=True)
                 if "no_trunc" in df_exp.columns:
@@ -60,7 +84,8 @@ def merge_all_experiments(args):
         filename = os.path.join(dir_conf, "merged_metrics.csv")
         if os.path.exists(filename):
             df = pd.read_csv(filename, index_col=0)
-            df_with_trunc = df_with_trunc.append(pd.Series(df.loc["with_trunc"], name=name_experiment))
+            if "with_trunc" in df.index:
+                df_with_trunc = df_with_trunc.append(pd.Series(df.loc["with_trunc"], name=name_experiment))
             df_no_trunc = df_no_trunc.append(pd.Series(df.loc["no_trunc"], name=name_experiment))
 
     columns_to_save = [col for col in args.columns_to_save if col in df_with_trunc.columns]
