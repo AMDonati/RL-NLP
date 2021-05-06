51443f7e97191affd96799110963c0f33a940d3e
Alice Martin <alice.martindonati@gmail.com>
2021-05-06 19:45:58 +0200

    slurm

--- src/scripts/sh/slurm/clevr_trunc_policy/clevr_pth0.1.sh
+++ src/scripts/sh/slurm/clevr_trunc_policy/clevr_pth0.1.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-#SBATCH --job-name=C-tr-pth0.05
+#SBATCH --job-name=C-tr-pth0.1
 #SBATCH --qos=qos_gpu-t3
 #SBATCH --ntasks=1
 #SBATCH --gres=gpu:3
 #SBATCH --array=1-5
 #SBATCH --cpus-per-task=16
 #SBATCH --mem-per-cpu=8g
-#SBATCH --output=slurm_out/clevr/trunc-pth0.05-%j.out
-#SBATCH --error=slurm_out/clevr/trunc-pth0.05-%j.err
+#SBATCH --output=slurm_out/clevr/trunc-pth0.1-%j.out
+#SBATCH --error=slurm_out/clevr/trunc-pth0.1-%j.err
 #SBATCH --time=20:00:00
 
 export TMPDIR=$JOBSCRATCH
@@ -42,6 +42,6 @@ REWARD_VOCAB="data/closure_vocab.json"
 set -x
 echo "now processing task id:: " ${SLURM_ARRAY_TASK_ID}
 OUT_PATH=output/RL/CLEVR_lm/trunc_policy/${SLURM_ARRAY_TASK_ID}
-srun python -u src/scripts/run.py -env $ENV_ -max_len $MAX_LEN -data_path $DATA_PATH -out_path ${OUT_PATH} -model $MODEL -update_every $UPDATE_EVERY -agent $AGENT -K_epochs $K_EPOCHS -eps_clip $EPS_CLIP -lr $LR -word_emb_size $WORD_EMB_SIZE -hidden_size $HIDDEN_SIZE -num_episodes_train $NUM_EPISODE_TRAIN -lm_path $LM_PATH -reward $REWARD -num_episodes_test $NUM_EPISODE_TEST -mask_answers 1 -grad_clip 1 -condition_answer $CONDITION_ANSWER -truncate_mode "proba_thr" -p_th 0.05 -debug $DEBUG -reward_vocab $REWARD_VOCAB -reward_path $REWARD_PATH -truncation_optim 1
+srun python -u src/scripts/run.py -env $ENV_ -max_len $MAX_LEN -data_path $DATA_PATH -out_path ${OUT_PATH} -model $MODEL -update_every $UPDATE_EVERY -agent $AGENT -K_epochs $K_EPOCHS -eps_clip $EPS_CLIP -lr $LR -word_emb_size $WORD_EMB_SIZE -hidden_size $HIDDEN_SIZE -num_episodes_train $NUM_EPISODE_TRAIN -lm_path $LM_PATH -reward $REWARD -num_episodes_test $NUM_EPISODE_TEST -mask_answers 1 -grad_clip 1 -condition_answer $CONDITION_ANSWER -truncate_mode "proba_thr" -p_th 0.1 -debug $DEBUG -reward_vocab $REWARD_VOCAB -reward_path $REWARD_PATH -truncation_optim 1
 
 
