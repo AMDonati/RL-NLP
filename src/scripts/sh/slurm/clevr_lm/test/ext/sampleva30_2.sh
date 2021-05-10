cb79e796e4586e8f96bd35750a35241ada1e3b4a
gqkc <guillaume.quispe@gmail.com>
2021-05-10 20:30:53 +0200

    clevr test trunc policy

---
+++ src/scripts/sh/slurm/clevr_lm/test/ext/sampleva30_2.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#SBATCH --job-name=C-T-G-sampleva
+#SBATCH --qos=qos_gpu-t3
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --gres=gpu:1
+#SBATCH --cpus-per-task=8
+#SBATCH --mem-per-cpu=8g
+#SBATCH --output=slurm_out/clevr/T-E-sampleva-%j.out
+#SBATCH --error=slurm_out/clevr/T-E-sampleva-%j.err
+#SBATCH --time=20:00:00
+
+
+export TMPDIR=$JOBSCRATCH
+module purge
+module load  pytorch-gpu/py3/1.7.1
+conda activate rl-nlp-2
+export PYTHONPATH=src:${PYTHONPATH}
+
+
+MODEL_PATH="output/RL/CLEVR_lm-ext/trunc_policy/2/clevr_vqa_PPO_answ-img_sampling_sample_va30_adam_0.001_ent0.01_epsclip0.02_graclip1.0/"
+NUM_EPISODES_TEST=5000
+
+set -x
+srun python -u src/scripts/test.py -models_path $MODEL_PATH -num_episodes_test $NUM_EPISODES_TEST -eval_modes sampling_ranking_lm
---
+++ src/scripts/sh/slurm/clevr_lm/test/pth0.05_3.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#SBATCH --job-name=C-T-pth0.05
+#SBATCH --qos=qos_gpu-t3
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --gres=gpu:1
+#SBATCH --cpus-per-task=8
+#SBATCH --mem-per-cpu=8g
+#SBATCH --output=slurm_out/clevr/T-pth0.05-%j.out
+#SBATCH --error=slurm_out/clevr/T-pth0.05-%j.err
+#SBATCH --time=20:00:00
+
+
+export TMPDIR=$JOBSCRATCH
+module purge
+module load  pytorch-gpu/py3/1.7.1
+conda activate rl-nlp-2
+export PYTHONPATH=src:${PYTHONPATH}
+
+
+MODEL_PATH="output/RL/CLEVR_lm/trunc_policy/3/clevr_vqa_PPO_answ-img_sampling_proba_thr0.05_adam_0.001_ent0.01_epsclip0.02_graclip1.0"
+NUM_EPISODES_TEST=5000
+
+set -x
+srun python -u src/scripts/test.py -models_path $MODEL_PATH -num_episodes_test $NUM_EPISODES_TEST -eval_modes sampling_ranking_lm
