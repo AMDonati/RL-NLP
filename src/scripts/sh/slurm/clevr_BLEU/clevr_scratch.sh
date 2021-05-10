2d21d70dc7f59fae9bcca093daf6a1c1bc368d67
Alice Martin <alice.martindonati@gmail.com>
2021-05-10 18:42:33 +0200

    slurm

--- src/scripts/sh/slurm/clevr_BLEU/clevr_scratch.sh
+++ src/scripts/sh/slurm/clevr_BLEU/clevr_scratch.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-#SBATCH --job-name=scratch
+#SBATCH --job-name=C-scratch-B
 #SBATCH --qos=qos_gpu-t3
-#SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --gres=gpu:1
-#SBATCH --cpus-per-task=8
+#SBATCH --gres=gpu:3
+#SBATCH --array=1-5
+#SBATCH --cpus-per-task=16
 #SBATCH --mem-per-cpu=8g
 #SBATCH --output=slurm_out/clevr/scratch-%j.out
 #SBATCH --error=slurm_out/clevr/scratch-%j.err
@@ -41,4 +41,5 @@ REWARD_PATH="output/vqa_model_film/model.pt"
 REWARD_VOCAB="data/closure_vocab.json"
 
 set -x
-srun python -u src/scripts/run.py -env $ENV_ -max_len $MAX_LEN -data_path $DATA_PATH -out_path $OUTPUT_PATH -model $MODEL -update_every $UPDATE_EVERY -agent $AGENT -K_epochs $K_EPOCHS -eps_clip $EPS_CLIP -lr $LR -word_emb_size $WORD_EMB_SIZE -hidden_size $HIDDEN_SIZE -num_episodes_train $NUM_EPISODE_TRAIN -lm_path $LM_PATH -reward $REWARD -num_episodes_test $NUM_EPISODE_TEST -mask_answers 1 -condition_answer $CONDITION_ANSWER -debug $DEBUG -reward_vocab $REWARD_VOCAB -reward_path $REWARD_PATH
+OUT_PATH=output/RL/CLEVR_lm/${SLURM_ARRAY_TASK_ID}
+srun python -u src/scripts/run.py -env $ENV_ -max_len $MAX_LEN -data_path $DATA_PATH -out_path ${OUT_PATH} -model $MODEL -update_every $UPDATE_EVERY -agent $AGENT -K_epochs $K_EPOCHS -eps_clip $EPS_CLIP -lr $LR -word_emb_size $WORD_EMB_SIZE -hidden_size $HIDDEN_SIZE -num_episodes_train $NUM_EPISODE_TRAIN -lm_path $LM_PATH -reward $REWARD -num_episodes_test $NUM_EPISODE_TEST -mask_answers 1 -condition_answer $CONDITION_ANSWER -debug $DEBUG -reward_vocab $REWARD_VOCAB -reward_path $REWARD_PATH -grad_clip 1
