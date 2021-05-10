4e56a92d7f7501814f24991358117de0adcea5ba
gqkc <guillaume.quispe@gmail.com>
2021-05-10 16:26:06 +0200

    off correction

--- src/scripts/sh/slurm/vqav2/test/off_pth_2.sh
+++ src/scripts/sh/slurm/vqav2/test/off_pth_2.sh
@@ -17,6 +17,6 @@ conda activate rl-nlp-2
 export PYTHONPATH=src:${PYTHONPATH}
 
 OUT_PATH=output/RL/VQAv2/off_pth/
-POLICY_PATH=output/RL/VQAv2/vqa_vilbert_rank2_PPO_answ-img_sampling_proba_thr0.005_adam_0.001_ent0.01_epsclip0.01_graclip5.0/20210428-124644
+POLICY_PATH="output/RL/VQAv2/vqa_vilbert_rank2_PPO_answ-img_sampling_proba_thr0.005_adam_0.001_ent0.01_epsclip0.01_graclip5.0/20210428-124644/checkpoints/model.pt"
 
-srun python -env vqa -max_len 10 -data_path data/vqa-v2/ -out_path $OUT_PATH -model lstm -update_every 128 -agent PPO -K_epochs 20 -eps_clip 0.01 -lr 0.001 -word_emb_size 128 -hidden_size 256 -num_episodes_train 100000 -lm_path output/vqa_lm_model/model.pt -reward vilbert_rank2 -num_episodes_test 20000 -mask_answers 1 -fusion average -condition_answer after_fusion -features_path data/vqa-v2/coco_trainval.lmdb/ -reward_vocab output/vilbert_vqav2/bert_base_6layer_6conect.json -reward_path output/vilbert_vqav2/model.bin -grad_clip 5 -truncate_mode proba_thr -p_th 0.005 -policy_path $POLICY_PATH
+srun python src/scripts/run.py -env vqa -max_len 10 -data_path data/vqa-v2/ -out_path $OUT_PATH -model lstm -update_every 128 -agent PPO -K_epochs 20 -eps_clip 0.01 -lr 0.001 -word_emb_size 128 -hidden_size 256 -num_episodes_train 0 -lm_path output/vqa_lm_model/model.pt -reward vilbert_rank2 -num_episodes_test 20000 -mask_answers 1 -fusion average -condition_answer after_fusion -features_path data/vqa-v2/coco_trainval.lmdb/ -reward_vocab output/vilbert_vqav2/bert_base_6layer_6conect.json -reward_path output/vilbert_vqav2/model.bin -grad_clip 5 -truncate_mode proba_thr -p_th 0.005 -policy_path $POLICY_PATH
