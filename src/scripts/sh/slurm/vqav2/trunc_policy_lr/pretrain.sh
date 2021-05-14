69340ea6fb40360dc6f29820b6c5bc9dd0f275ef
gqkc <guillaume.quispe@gmail.com>
2021-05-14 20:21:54 +0200

    pretrain

--- src/scripts/sh/slurm/vqav2/trunc_policy_lr/pretrain.sh
+++ src/scripts/sh/slurm/vqav2/trunc_policy_lr/pretrain.sh
@@ -1,21 +1,5 @@
 #!/bin/bash
-#SBATCH --job-name=pretrain
-#SBATCH --qos=qos_gpu-t4
-#SBATCH --ntasks=1
-#SBATCH --gres=gpu:1
-#SBATCH --cpus-per-task=8
-#SBATCH --mem-per-cpu=8g
-#SBATCH --output=slurm_out/vqa_lr/pretrain%j.out
-#SBATCH --error=slurm_out/vqa_lr/pretrain%j.err
-#SBATCH --time=100:00:00
-
-export TMPDIR=$JOBSCRATCH
-module purge
-module load  pytorch-gpu/py3/1.7.1
-conda activate rl-nlp-2
-
 export PYTHONPATH=src:${PYTHONPATH}
-
 
 DATA_PATH="data/vqa-v2/"
 FEATURES_PATH="data/vqa-v2/coco_trainval.lmdb/"
@@ -29,13 +13,12 @@ VILBERT_PATH="output/vilbert_vqav2/model.bin"
 K_EPOCHS=20
 MAX_LEN=10
 UPDATE_EVERY=128
-DEBUG="0,20000"
 NUM_EPISODE_TRAIN=100000
 NUM_EPISODE_TEST=20000
 ENV_="vqa"
 MODEL="lstm"
 AGENT="PPO"
-LR=0.00001
+LR=0.000001
 WORD_EMB_SIZE=128
 HIDDEN_SIZE=256
 EPS_CLIP=0.01
@@ -43,5 +26,5 @@ REWARD="vilbert_rank2"
 FUSION="average"
 CONDITION_ANSWER="after_fusion"
 
-OUT_PATH=output/RL/VQAv2_full/truncated_policy_lr/
-srun python -u src/scripts/run.py -env $ENV_ -max_len $MAX_LEN -data_path $DATA_PATH -out_path $OUTPUT_PATH -model $MODEL -update_every $UPDATE_EVERY -agent $AGENT -K_epochs $K_EPOCHS -eps_clip $EPS_CLIP -lr $LR -word_emb_size $WORD_EMB_SIZE -hidden_size $HIDDEN_SIZE -num_episodes_train $NUM_EPISODE_TRAIN -lm_path $LM_PATH -reward $REWARD -num_episodes_test $NUM_EPISODE_TEST -mask_answers 1 -fusion $FUSION -condition_answer $CONDITION_ANSWER -features_path $FEATURES_PATH -reward_vocab $VILBERT_VOCAB -reward_path $VILBERT_PATH -policy_path $POLICY_PATH -grad_clip 0.5 -debug $DEBUG
+OUT_PATH=output/RL/VQAV2_end
+python src/scripts/run.py -env $ENV_ -max_len $MAX_LEN -data_path $DATA_PATH -out_path $OUTPUT_PATH -model $MODEL -update_every $UPDATE_EVERY -agent $AGENT -K_epochs $K_EPOCHS -eps_clip $EPS_CLIP -lr $LR -word_emb_size $WORD_EMB_SIZE -hidden_size $HIDDEN_SIZE -num_episodes_train $NUM_EPISODE_TRAIN -lm_path $LM_PATH -reward $REWARD -num_episodes_test $NUM_EPISODE_TEST -mask_answers 1 -fusion $FUSION -condition_answer $CONDITION_ANSWER -features_path $FEATURES_PATH -reward_vocab $VILBERT_VOCAB -reward_path $VILBERT_PATH -policy_path $POLICY_PATH
