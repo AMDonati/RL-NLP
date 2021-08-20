64d161f0add957c004ba43eada6c05efeb69f2fc
gqkc <guillaume.quispe@gmail.com>
2021-08-20 17:28:28 +0200

    preprocess

---
+++ src/scripts/sh/slurm/vqav2_data_pp_part_2.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#SBATCH --job-name=debug-vqa
+#SBATCH --qos=qos_gpu-t3
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --gres=gpu:1
+#SBATCH --cpus-per-task=16
+#SBATCH --mem-per-cpu=8g
+#SBATCH --output=slurm_out/debugvqa%j.out
+#SBATCH --error=slurm_out/debugvqa%j.err
+#SBATCH --time=20:00:00
+#SBATCH -A ktz@gpu
+
+
+module purge
+module load  pytorch-gpu/py3/1.7.1
+conda activate rl-nlp-2
+
+export PYTHONPATH=src:${PYTHONPATH}
+
+set -x
+srun python -u src/preprocessing/preprocess_vqa_dataset.py -data_path "data/vqa-v2" -features_path "data/vqa-v2/coco_trainval.lmdb" -vocab_path "none" -min_split 0
